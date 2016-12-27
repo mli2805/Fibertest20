21cd7d1453e8fe4391b31e5969fc20ff51b9b11b
mli2805 <mli2805@gmail.com>
2016-12-27 15:37:59 +0300

    cleared usings

--- WpfClient/ViewModels/AddEquipmentViewModel.cs
+++ WpfClient/ViewModels/AddEquipmentViewModel.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
 using Caliburn.Micro;
 using Iit.Fibertest.Graph;
 using Iit.Fibertest.Graph.Commands;
