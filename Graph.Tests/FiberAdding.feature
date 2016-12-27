084ed515080b9e4921d66569a28aa6cf53df34c7
mli2805 <mli2805@gmail.com>
2016-12-27 16:53:07 +0300

    new feature started

---
+++ Graph.Tests/FiberAdding.feature
@@ -0,0 +1,9 @@
+﻿Feature: FiberAdding
+
+Background: 
+	Given Left and right nodes created
+
+@mytag
+Scenario: Add fiber
+	When User clicked Add fiber
+	Then New event persisted
---
+++ Graph.Tests/FiberAddingSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Graph.Tests
+{
+    [Binding]
+    public sealed class FiberAddingSteps
+    {
+        private readonly SystemUnderTest _sut = new SystemUnderTest();
+        private Guid _leftNodeId;
+        private Guid _rightNodeId;
+        private int _cutOff;
+
+        [Given(@"Left and right nodes created")]
+        public void GivenALeftAndRightNodesCreated()
+        {
+            _leftNodeId = _sut.AddNode();
+            _cutOff = _sut.CurrentEventNumber;
+            _rightNodeId = _sut.AddNode();
+            _cutOff = _sut.CurrentEventNumber;
+        }
+
+        [When(@"User clicked Add fiber")]
+        public void WhenUserClickedAddFiber()
+        {
+            //
+        }
+
+        [Then(@"New event persisted")]
+        public void ThenNewEventPersisted()
+        {
+            _sut.CurrentEventNumber.Should().BeGreaterThan(_cutOff);
+        }
+
+
+    }
+}
