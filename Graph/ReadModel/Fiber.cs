f39b44ae1741fb65080bdb9a945c9e3bbe958348
mli2805 <mli2805@gmail.com>
2016-12-28 16:52:19 +0300

    AddRtu

--- Graph/ReadModel/Fiber.cs
+++ Graph/ReadModel/Fiber.cs
@@ -7,5 +7,10 @@ namespace Iit.Fibertest.Graph
         public Guid Id { get; set; }
         public Guid Node1 { get; set; }
         public Guid Node2 { get; set; }
+
+        public double GpsLength { get; set; }
+        public bool ShouldGpsLengthBeChangedOnNodeMove { get; set; } = true;
+
+        public double OpticalLength { get; set; }
     }
 }
---
+++ Graph/ReadModel/Rtu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iit.Fibertest.Graph
+{
+    public class Rtu
+    {
+        public Guid Id { get; set; }
+        public Guid NodeId { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+    }
+}
