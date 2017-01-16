987e8624b5d492910a1334f1537708704349fb55
mli2805 <mli2805@gmail.com>
2017-01-16 22:35:27 +0300

    test coverage improvement

--- Graph/DomainModel/Rtu.cs
+++ Graph/DomainModel/Rtu.cs
@@ -7,6 +7,5 @@ namespace Iit.Fibertest.Graph
         public Guid Id { get; set; }
         public Guid NodeId { get; set; }
         public string Title { get; set; }
-        public string Comment { get; set; }
     }
 }
