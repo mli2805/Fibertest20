be311511a6fcb58605f7f0edf796be18787ce6f6
mli2805 <mli2805@gmail.com>
2017-02-01 22:49:24 +0300

    MarkerControlMouseHandling in separate file

--- TestBench/MarkerControl/MarkerControl.xaml.cs
+++ TestBench/MarkerControl/MarkerControl.xaml.cs
@@ -15,9 +15,9 @@ using Iit.Fibertest.TestBench.Properties;
 namespace Iit.Fibertest.TestBench
 {
     /// <summary>
-    /// Interaction logic for MarkerControl.xaml
-    /// </summary>
-    public partial class MarkerControl : INotifyPropertyChanged
+        /// Interaction logic for MarkerControl.xaml
+        /// </summary>
+        public partial class MarkerControl : INotifyPropertyChanged
     {
         private Popup _popup;
         private Label _label;
@@ -70,22 +70,6 @@ namespace Iit.Fibertest.TestBench
             _popup = new Popup { Placement = PlacementMode.Mouse, Child = _label, };
         }
 
-        private void MarkerControl_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            ContextMenu contextMenu;
-            if (_type == EquipmentType.Rtu)
-                contextMenu = FindResource("RtuContextMenu") as ContextMenu;
-            else
-                contextMenu = FindResource("NodeContextMenu") as ContextMenu;
-            if (contextMenu == null) return;
-
-            var item = new MenuItem() {Header = "from code", Tag = 78};
-            contextMenu.Items.Add(item);
-
-            //TODO define which menu items are disabled
-            contextMenu.IsOpen = true;
-        }
-
         private void AssignBitmapImage(Image pictogram)
         {
             pictogram.Width = _type == EquipmentType.Rtu ? 40 : 8;
@@ -121,99 +105,6 @@ namespace Iit.Fibertest.TestBench
             _label = null;
         }
 
-        // происходит при запуске приложения поэтому используется для инициализации offset
-        // offset задает смещение пиктограммы относительно координат
-        void MarkerControl_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            _marker.Offset = new Point(-e.NewSize.Width / 2, -e.NewSize.Height / 2);
-        }
-
-        // при нажатом Ctrl левая кнопка таскает данный маркер
-        private void MarkerControl_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed && IsMouseCaptured && (Keyboard.Modifiers & ModifierKeys.Control) != 0)
-            {
-                _popup.IsOpen = false;
-                DragMarkerWithItsFibers(e);
-            }
-
-            e.Handled = true;
-        }
-
-        private void DragMarkerWithItsFibers(MouseEventArgs e)
-        {
-            Point p = e.GetPosition(_mainMap);
-            _marker.Position = _mainMap.FromLocalToLatLng((int)(p.X), (int)(p.Y));
-
-            foreach (var route in _mainMap.Markers.Where(m => m is GMapRoute))
-            {
-                var r = (GMapRoute)route;
-                if (r.LeftId != _marker.Id && r.RightId != _marker.Id)
-                    continue;
-                if (r.LeftId == _marker.Id)
-                    r.Points[0] = _marker.Position;
-                if (r.RightId == _marker.Id)
-                    r.Points[1] = _marker.Position;
-                r.RegenerateShape(_mainMap);
-            }
-        }
-
-        void MarkerControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (!IsMouseCaptured && (Keyboard.Modifiers & ModifierKeys.Control) != 0)
-            {
-                Mouse.Capture(this);
-            }
-            e.Handled = true;
-        }
-
-        void MarkerControl_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (IsMouseCaptured)
-            {
-                Mouse.Capture(null);
-                Command = new MoveNode() { Id = _marker.Id, Latitude = _marker.Position.Lat, Longitude = _marker.Position.Lng };
-            }
-
-            if (_mainMap.IsInFiberCreationMode)
-            {
-                EndFiberCreation();
-            }
-            e.Handled = true;
-        }
-
-        private void EndFiberCreation()
-        {
-            _mainMap.IsInFiberCreationMode = false;
-            // it was a temporary fiber for creation purposes only
-            _mainMap.Markers.Remove(_mainMap.Markers.Single(m => m.Id == _mainMap.FiberUnderCreation));
-
-            if (!_mainMap.IsFiberWithNodes)
-                Command = new AddFiber() { Node1 = _mainMap.StartNode.Id, Node2 = _marker.Id };
-            else
-                Command = new AddFiberWithNodes() { Node1 = _mainMap.StartNode.Id, Node2 = _marker.Id, };
-
-            _mainMap.FiberUnderCreation = Guid.Empty;
-            Cursor = Cursors.Arrow;
-        }
-
-
-        void MarkerControl_MouseLeave(object sender, MouseEventArgs e)
-        {
-            _marker.ZIndex -= 10000;
-            Cursor = Cursors.Arrow;
-            if (!string.IsNullOrEmpty(_title))
-                _popup.IsOpen = false;
-        }
-
-        void MarkerControl_MouseEnter(object sender, MouseEventArgs e)
-        {
-            _marker.ZIndex += 10000;
-            Cursor = Cursors.Hand;
-
-            if (!string.IsNullOrEmpty(_title))
-                _popup.IsOpen = true;
-        }
 
         public bool IsMenuItemRemoveNodeEnabled { get; set; }
         private void MarkerContextMenuOnClick(object sender, RoutedEventArgs e)
