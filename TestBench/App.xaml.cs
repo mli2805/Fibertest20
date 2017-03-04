c41e1a7506e9b5f8983bdca50a4912bc458847fe
marhoily <marhoily@homtail.com>
2017-03-04 15:15:49 +0300

    Handle crashes

--- TestBench/App.xaml.cs
+++ TestBench/App.xaml.cs
@@ -1,13 +1,35 @@
-﻿namespace Iit.Fibertest.TestBench
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Iit.Fibertest.TestBench
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        static App()
+        {
+            AppDomain.CurrentDomain.UnhandledException +=
+                (s, e) => Log((Exception)e.ExceptionObject);
+
+        }
         public App()
         {
             InitializeComponent();
+            DispatcherUnhandledException += (sender, e) => Log(e.Exception);
+        }
+
+        private static void Log(Exception eException)
+        {
+            File.AppendAllText("crash.txt",
+                Environment.NewLine +
+                Environment.NewLine +
+                Environment.NewLine +
+                eException);
+            MessageBox.Show(eException.ToString());
         }
     }
 }
