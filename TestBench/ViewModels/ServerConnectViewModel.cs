0b084287e86f0bd5ef07415481e4f3aacfae5c03
mli2805 <mli2805@gmail.com>
2017-03-28 22:45:28 +0300

    ServerConnectViewModel little changes

--- TestBench/ViewModels/ServerConnectViewModel.cs
+++ TestBench/ViewModels/ServerConnectViewModel.cs
@@ -1,17 +1,33 @@
 ﻿using Caliburn.Micro;
 using Iit.Fibertest.Graph;
+using Iit.Fibertest.StringResources;
 
 namespace Iit.Fibertest.TestBench
 {
     public class ServerConnectViewModel : Screen
     {
-        public NetAddressTestViewModel ServerConnectionTestViewModel;
+        public NetAddressTestViewModel ServerConnectionTestViewModel { get; set; }
 
         public ServerConnectViewModel(IniFile iniFile)
         {
             var serverIp = iniFile.Read(IniSection.General, IniKey.ServerIp, @"192.168.96.21");
 
-            ServerConnectionTestViewModel = new NetAddressTestViewModel(new NetAddress(serverIp, 118331));
+            ServerConnectionTestViewModel = new NetAddressTestViewModel(new NetAddress(serverIp, 11831));
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            DisplayName = Resources.SID_Server;
+        }
+
+        public void Save()
+        {
+            TryClose(true);
+        }
+
+        public void Cancel()
+        {
+            TryClose(false);
         }
     }
 }
