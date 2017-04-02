59daac76efc393722dc9a8ef749b91cfbf0e760f
mli2805 <mli2805@gmail.com>
2017-04-02 19:06:21 +0300

    user add/edit view

---
+++ TestBench/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+
+namespace Iit.Fibertest.TestBench
+{
+    public class UserViewModel : Screen
+    {
+        public User User { get; set; }
+        private readonly bool _isCreateNewUserMode;
+
+        public UserViewModel(bool IsCreateNewUserMode, User user)
+        {
+            User = user;
+            _isCreateNewUserMode = IsCreateNewUserMode;
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            DisplayName = "User";
+        }
+    }
+}
---
+++ TestBench/Views/Users/UserView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Iit.Fibertest.TestBench.Users
+{
+    /// <summary>
+    /// Interaction logic for UserView.xaml
+    /// </summary>
+    public partial class UserView
+    {
+        public UserView()
+        {
+            InitializeComponent();
+        }
+    }
+}
