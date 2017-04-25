8259ca22393fae751955a4f6c4ce3160679f40cf
mli2805 <mli2805@gmail.com>
2017-04-25 21:11:47 +0300

    landmark view continues

--- WpfClient/ViewModels/Landmarks/LandmarkViewModel.cs
+++ WpfClient/ViewModels/Landmarks/LandmarkViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using Caliburn.Micro;
+using Iit.Fibertest.Graph;
 
 namespace Iit.Fibertest.Client
 {
     public class LandmarkViewModel : Screen
     {
+        public string NodeTitle { get; set; }
+        public string EquipmentTitle { get; set; }
+        public GpsInputViewModel GpsInputViewModel { get; set; }
+
         public LandmarkViewModel()
         {
         }
 
         public void Initialize(Landmark landmark)
         {
-            DisplayName = landmark.NodeTitle;
+            DisplayName = $"Landmark {landmark.EquipmentTitle}";
+            NodeTitle = landmark.NodeTitle;
+            EquipmentTitle = landmark.EquipmentTitle;
+
+            GpsInputViewModel = new GpsInputViewModel(GpsInputMode.DegreesMinutesAndSeconds, landmark.GpsCoors);
         }
     }
 }
