51197e35b4a283006d3534fe708894e41c204a24
mli2805 <mli2805@gmail.com>
2017-06-13 15:54:19 +0300

    configuration for rtu-release-x86, wcf service available by ip

--- RtuWpfExample/WcfClientViewModel.cs
+++ RtuWpfExample/WcfClientViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ServiceModel;
+using System.Windows;
 using Caliburn.Micro;
 using Iit.Fibertest.RtuWpfExample.ServiceReference1;
 
@@ -6,13 +8,37 @@ namespace Iit.Fibertest.RtuWpfExample
 {
     public class WcfClientViewModel : Screen
     {
-
+        private RtuWcfServiceClient _rtuWcfServiceClient;
 
         public void WcfTest()
         {
-            RtuWcfServiceClient myService = new RtuWcfServiceClient();
-            MessageBox.Show(myService.GetData(123), @"My WCF Service");
-            myService.Close();
+                        StartRtuWcfServiceClient("192.168.96.179");
+//            _rtuWcfServiceClient = new RtuWcfServiceClient();
+
+            MessageBox.Show(_rtuWcfServiceClient.GetData(123), @"My WCF Service");
+            _rtuWcfServiceClient.Close();
+        }
+
+
+        private void StartRtuWcfServiceClient(string address)
+        {
+            try
+            {
+                var uriString = @"net.tcp://" + address + @":8523/RtuWcfService";
+                var netTcpBinding = new NetTcpBinding();
+                netTcpBinding.Security.Mode = SecurityMode.None;
+                netTcpBinding.ReceiveTimeout = new TimeSpan(0, 15, 0);
+                netTcpBinding.SendTimeout = new TimeSpan(0, 15, 0);
+
+                netTcpBinding.MaxBufferSize = 4096000; //4M
+                _rtuWcfServiceClient = new RtuWcfServiceClient(netTcpBinding, new EndpointAddress(new Uri(uriString)));
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
