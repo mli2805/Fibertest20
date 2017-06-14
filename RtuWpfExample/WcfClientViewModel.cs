bb8d9e5de2cd6a1ec959a75f33a8143cff151235
mli2805 <mli2805@gmail.com>
2017-06-14 15:11:49 +0300

    rtu service tcp port 11842

--- RtuWpfExample/WcfClientViewModel.cs
+++ RtuWpfExample/WcfClientViewModel.cs
@@ -12,46 +12,74 @@ namespace Iit.Fibertest.RtuWpfExample
         private readonly IniFile _iniFile35;
         private readonly Logger35 _rtuLogger;
         private RtuWcfServiceClient _rtuWcfServiceClient;
-        private readonly string _rtuServiceIp;
+        private string _rtuServiceIp;
+
+        public string RtuServiceIp
+        {
+            get { return _rtuServiceIp; }
+            set
+            {
+                if (value == _rtuServiceIp) return;
+                _rtuServiceIp = value;
+                NotifyOfPropertyChange();
+            }
+        }
 
         public WcfClientViewModel(IniFile iniFile35, Logger35 rtuLogger)
         {
             _iniFile35 = iniFile35;
             _rtuLogger = rtuLogger;
-            _rtuServiceIp = _iniFile35.Read(IniSection.General, IniKey.RtuServiceIp, @"192.168.96.53");
+            RtuServiceIp = _iniFile35.Read(IniSection.General, IniKey.RtuServiceIp, @"192.168.96.53");
         }
 
         public void WcfTest()
         {
-            StartRtuWcfServiceClient(_rtuServiceIp);
+            _rtuWcfServiceClient = CreateRtuWcfServiceClient(RtuServiceIp);
+            if (_rtuWcfServiceClient == null)
+                return;
 
+            _rtuLogger.AppendLine($@"Wcf client to {RtuServiceIp} created");
 
-            var test = _rtuWcfServiceClient.GetData(456);
-            MessageBox.Show(test, @"My WCF Service");
-            _rtuWcfServiceClient.Close();
+            CheckWcf();
         }
 
+        private void CheckWcf()
+        {
+            _iniFile35.Write(IniSection.General, IniKey.RtuServiceIp, RtuServiceIp);
+            try
+            {
+                _rtuWcfServiceClient.Open();
+                var test = _rtuWcfServiceClient.GetData(456);
+                MessageBox.Show(test, @"My WCF Service");
+                _rtuLogger.AppendLine($@"Wcf connection with {_rtuWcfServiceClient.Endpoint.Address} established successfully");
+                _rtuWcfServiceClient.Close();
+            }
+            catch (Exception e)
+            {
+                _rtuLogger.AppendLine($@"Can't establish connection with {_rtuWcfServiceClient.Endpoint.Address}");
+                MessageBox.Show(e.Message, @"My WCF Service");
+            }
+        }
 
-        private void StartRtuWcfServiceClient(string address)
+        private RtuWcfServiceClient CreateRtuWcfServiceClient(string address)
         {
             try
             {
-                var uriString = @"net.tcp://" + address + @":8523/RtuWcfService";
+                var uriString = @"net.tcp://" + address + @":11842/RtuWcfService";
                 var netTcpBinding = new NetTcpBinding();
                 netTcpBinding.Security.Mode = SecurityMode.None;
                 netTcpBinding.ReceiveTimeout = new TimeSpan(0, 15, 0);
                 netTcpBinding.SendTimeout = new TimeSpan(0, 15, 0);
-                netTcpBinding.OpenTimeout = new TimeSpan(0,1,0);
+                netTcpBinding.OpenTimeout = new TimeSpan(0, 1, 0);
 
                 netTcpBinding.MaxBufferSize = 4096000; //4M
-                _rtuWcfServiceClient = new RtuWcfServiceClient(netTcpBinding, new EndpointAddress(new Uri(uriString)));
-                _rtuLogger.AppendLine($"Wcf connection with {address} established");
+                return new RtuWcfServiceClient(netTcpBinding, new EndpointAddress(new Uri(uriString)));
             }
             catch (Exception e)
             {
-                _rtuLogger.AppendLine($"Can't establish wcf connection with {address}");
                 _rtuLogger.AppendLine(e.Message);
-                throw;
+                MessageBox.Show(e.Message, @"My WCF Service");
+                return null;
             }
         }
     }
