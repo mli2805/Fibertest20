e8ad95a657abf416c8b74635bd08c8df1d872de1
mli2805 <mli2805@gmail.com>
2017-07-04 19:01:26 +0300

    D4R wcf service

---
+++ D4R_WcfService/D4RWcfService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace D4R_WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "D4R_WcfService" in both code and config file together.
+    public class D4RWcfService : ID4RWcfService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ D4R_WcfService/ID4RWcfService.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace D4R_WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ID4R_WcfService" in both code and config file together.
+    [ServiceContract]
+    public interface ID4RWcfService
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool _boolValue = true;
+        string _stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return _boolValue; }
+            set { _boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return _stringValue; }
+            set { _stringValue = value; }
+        }
+    }
+}
