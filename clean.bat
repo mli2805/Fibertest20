1b44eee36b8a6dd6d518c91651242f96267602c1
mli2805 <mli2805@gmail.com>
2017-07-11 16:21:36 +0300

    string resources are shared

---
+++ clean.bat
@@ -0,0 +1,9 @@
+set OLDDIR=%CD%
+
+cd %~dp0
+
+
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+cd %OLDDIR%
