04153e00233b0613ac09d69cd63cc2d99c2a9039
mli2805 <mli2805@gmail.com>
2017-09-24 09:28:11 +0300

    ncrunch

---
+++ Graph.Tests/Sut/FakeWcfServiceForClient.cs
@@ -0,0 +1,62 @@
+﻿using Dto;
+
+namespace Graph.Tests
+{
+    public class FakeWcfServiceForClient : WcfServiceForClientLibrary.IWcfServiceForClient
+    {
+        public string SendCommand(string json)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string[] GetEvents(int revision)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegisterClient(RegisterClientDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UnRegisterClient(UnRegisterClientDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckServerConnection(CheckServerConnectionDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckRtuConnection(CheckRtuConnectionDto rtuAddress)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool InitializeRtu(InitializeRtuDto rtu)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool StartMonitoring(StartMonitoringDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool StopMonitoring(StopMonitoringDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ApplyMonitoringSettings(ApplyMonitoringSettingsDto settings)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AssignBaseRef(AssignBaseRefDto baseRef)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
