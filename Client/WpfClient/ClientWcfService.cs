45492a0aa0674334f25a56b2c9847e6643e60ac2
mli2805 <mli2805@gmail.com>
2017-11-10 22:20:56 +0300

    Warning cleanup

--- Client/WpfClient/ClientWcfService.cs
+++ Client/WpfClient/ClientWcfService.cs
@@ -2,21 +2,14 @@
 using System.Threading.Tasks;
 using ClientWcfServiceInterface;
 using Iit.Fibertest.Dto;
-using Iit.Fibertest.UtilsLib;
 
 namespace Iit.Fibertest.Client
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ClientWcfService : IClientWcfService
     {
-        private readonly IMyLog _logFile;
         public event OnMessageReceived MessageReceived;
         public delegate void OnMessageReceived(object e);
-
-        public ClientWcfService(IMyLog logFile)
-        {
-            _logFile = logFile;
-        }
 
         public void ProcessRtuCurrentMonitoringStep(KnowRtuCurrentMonitoringStepDto dto)
         {
