af6b8c0cfca1482ace2c235df27a3066f5f0981c
mli2805 <mli2805@gmail.com>
2017-11-16 08:51:07 +0300

    folder reordering

---
+++ Client/WpfClient/Forms/Shell/SysEventsControl/OpticalEventsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Iit.Fibertest.Client.Forms.Shell.SysEventsControl
+{
+    /// <summary>
+    /// Interaction logic for OpticalEventsView.xaml
+    /// </summary>
+    public partial class OpticalEventsView : UserControl
+    {
+        public OpticalEventsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
