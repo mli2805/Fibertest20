a0f4469e54f511ad4dd6dd282bd4f11b0a8fce81
mli2805 <mli2805@gmail.com>
2017-11-16 14:37:34 +0300

    fields added

--- SharedLibs/Dto/Db/OpticalEvent.cs
+++ SharedLibs/Dto/Db/OpticalEvent.cs
@@ -9,9 +9,11 @@ namespace Iit.Fibertest.Dto
         public DateTime EventTimestamp { get; set; }
         public Guid RtuId { get; set; }
         public Guid TraceId { get; set; }
+        public BaseRefType BaseRefType { get; set; }
         public FiberState TraceState { get; set; }
         public EventStatus EventStatus { get; set; }
         public DateTime StatusTimestamp { get; set; }
         public int StatusUserId { get; set; }
+        public string Comment { get; set; }
     }
 }
