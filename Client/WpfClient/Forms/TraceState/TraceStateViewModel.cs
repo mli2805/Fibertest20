dd6bdbc87886645097a202ab42890e5a252621a9
mli2805 <mli2805@gmail.com>
2017-11-23 16:59:34 +0300

    TraceStateView continues

--- Client/WpfClient/Forms/TraceState/TraceStateViewModel.cs
+++ Client/WpfClient/Forms/TraceState/TraceStateViewModel.cs
@@ -1,9 +1,24 @@
-﻿using Caliburn.Micro;
+﻿using System.Windows.Media;
+using Caliburn.Micro;
+using Iit.Fibertest.Dto;
 
 namespace Iit.Fibertest.Client
 {
     public class TraceStateViewModel : Screen
     {
+        public string TraceTitle { get; set; }
+        public string RtuTitle { get; set; }
+        public string PortTitle { get; set; }
+        public FiberState TraceState { get; set; }
+        public BaseRefType BaseRefType { get; set; }
+
+        //public Brush TraceStateBrush => BaseRefType == BaseRefType.Fast && TraceState != FiberState.Ok ? 
+        public Brush TraceStateBrush => TraceState.GetBrush(isForeground: true);
+        public void Initialize(int sorFileId)
+        {
+            TraceTitle = "bla!";
+            TraceState = FiberState.Suspicion;
+        }
         protected override void OnViewLoaded(object view)
         {
             DisplayName = "Trace state";
