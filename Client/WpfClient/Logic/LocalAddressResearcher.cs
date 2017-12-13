6fc60e98a0dfce3091ecd537437e79614686aca5
mli2805 <mli2805@gmail.com>
2017-12-13 22:31:25 +0300

    defines client's local address after user enter server's

---
+++ Client/WpfClient/Logic/LocalAddressResearcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Iit.Fibertest.Client
+{
+    public static class LocalAddressResearcher
+    {
+        public static string GetLocalAddressToConnectServer(string serverAddress)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect(serverAddress, 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                return endPoint?.Address.ToString();
+            }
+        }
+
+        /*
+        private static void GetAllLocalAddresses()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                var family = "";
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    family = @"IPv4";
+                if (ip.AddressFamily == AddressFamily.InterNetworkV6)
+                    family = @"IPv6";
+                Console.WriteLine($@"{family}:   {ip}");
+            }
+        }
+        */
+    }
+}
