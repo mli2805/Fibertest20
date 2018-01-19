46fe5e374941f01d7be00becdbf6ffc31dc706e8
mli2805 <mli2805@gmail.com>
2018-01-19 16:44:04 +0300

    warning cleanup

--- Client/WpfClient/Forms/Shell/TreeOfRtuControl/TraceLeaf/TraceLeaf.cs
+++ Client/WpfClient/Forms/Shell/TreeOfRtuControl/TraceLeaf/TraceLeaf.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using Iit.Fibertest.Dto;
 using Iit.Fibertest.StringResources;
 
