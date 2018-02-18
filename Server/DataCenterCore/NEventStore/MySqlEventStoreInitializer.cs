a06ad0962b5eb2b7d92c74b434ad19633956f350
mli2805 <mli2805@gmail.com>
2018-02-18 14:58:08 +0300

    ft20graph created automatically

--- Server/DataCenterCore/NEventStore/MySqlEventStoreInitializer.cs
+++ Server/DataCenterCore/NEventStore/MySqlEventStoreInitializer.cs
@@ -1,5 +1,6 @@
 using System;
 using Iit.Fibertest.UtilsLib;
+using MySql.Data.MySqlClient;
 using NEventStore;
 using NEventStore.Persistence.Sql.SqlDialects;
 
@@ -9,6 +10,7 @@ namespace Iit.Fibertest.DataCenterCore
     {
         public IStoreEvents Init(IMyLog logFile)
         {
+            CreateDatabaseIfNotExists(logFile);
             try
             {
                 var eventStore = Wireup.Init()
@@ -24,8 +26,24 @@ namespace Iit.Fibertest.DataCenterCore
             catch (Exception e)
             {
                 logFile.AppendLine("MySqlEventStoreInitializer exception : " + e.Message);
-                logFile.AppendLine("Empty schema ft20graph need to be created manually beforehand");
                 return null;
+            }
+        }
+
+        private void CreateDatabaseIfNotExists(IMyLog logFile)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("server=localhost;user id=root;password=root;");
+                MySqlCommand command = new MySqlCommand("create database if not exists ft20graph;", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                logFile.AppendLine(e.Message);
+                throw;
             }
         }
     }
