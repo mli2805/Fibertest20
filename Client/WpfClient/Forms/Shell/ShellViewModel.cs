938b98a6ec3037e22bde6988bbab392161323392
mli2805 <mli2805@gmail.com>
2018-02-22 14:50:10 +0300

    application exit changed

--- Client/WpfClient/Forms/Shell/ShellViewModel.cs
+++ Client/WpfClient/Forms/Shell/ShellViewModel.cs
@@ -69,13 +69,14 @@ namespace Iit.Fibertest.Client
 
         public override void CanClose(Action<bool> callback)
         {
-            _clientPollerCts.Cancel();
-            _c2DWcfManager?.UnregisterClientAsync(new UnRegisterClientDto());
-            _iniFile.Write(IniSection.Map, IniKey.Zoom, GraphReadModel.Zoom);
-            _iniFile.Write(IniSection.Map, IniKey.CenterLatitude, GraphReadModel.CenterForIni.Lat);
-            _iniFile.Write(IniSection.Map, IniKey.CenterLongitude, GraphReadModel.CenterForIni.Lng);
-            _logFile.AppendLine(@"Client application finished!");
-            Thread.Sleep(TimeSpan.FromMilliseconds(1000));
+            Task.Factory.StartNew(() => {
+                _clientPollerCts.Cancel();
+                _c2DWcfManager?.UnregisterClientAsync(new UnRegisterClientDto());
+                _iniFile.Write(IniSection.Map, IniKey.Zoom, GraphReadModel.Zoom);
+                _iniFile.Write(IniSection.Map, IniKey.CenterLatitude, GraphReadModel.CenterForIni.Lat);
+                _iniFile.Write(IniSection.Map, IniKey.CenterLongitude, GraphReadModel.CenterForIni.Lng);
+                _logFile.AppendLine(@"Client application finished!");
+            });
             base.CanClose(callback);
         }
 
