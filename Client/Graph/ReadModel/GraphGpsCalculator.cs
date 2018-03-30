fce72ec8427ca5c04e556fdab9fd37c2f5911d36
mli2805 <mli2805@gmail.com>
2018-03-30 08:55:39 +0300

    fixed bug with fiber gps size calculation (adjustment points)

--- Client/Graph/ReadModel/GraphGpsCalculator.cs
+++ Client/Graph/ReadModel/GraphGpsCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Iit.Fibertest.Dto;
 
 namespace Iit.Fibertest.Graph
@@ -15,19 +16,19 @@ namespace Iit.Fibertest.Graph
         public double CalculateTraceGpsLengthKm(Trace trace)
         {
             double result = 0;
-            for (int i = 0; i < trace.Nodes.Count-1; i++)
+            for (int i = 0; i < trace.Nodes.Count - 1; i++)
             {
                 var node1 = _readModel.Nodes.FirstOrDefault(n => n.Id == trace.Nodes[i]);
                 if (node1 == null) return 0;
-                var node2 = _readModel.Nodes.FirstOrDefault(n => n.Id == trace.Nodes[i+1]);
+                var node2 = _readModel.Nodes.FirstOrDefault(n => n.Id == trace.Nodes[i + 1]);
                 if (node2 == null) return 0;
 
                 var equipment1 = i == 0
-                    ? new Equipment() { Type = EquipmentType.Rtu, CableReserveLeft = 0, CableReserveRight = 0}
+                    ? new Equipment() { Type = EquipmentType.Rtu, CableReserveLeft = 0, CableReserveRight = 0 }
                     : _readModel.Equipments.FirstOrDefault(e => e.Id == trace.Equipments[i]);
-                var equipment2 = _readModel.Equipments.FirstOrDefault(e => e.Id == trace.Equipments[i+1]);
+                var equipment2 = _readModel.Equipments.FirstOrDefault(e => e.Id == trace.Equipments[i + 1]);
 
-                result = result + 
+                result = result +
                          GpsCalculator.GetDistanceBetweenPointLatLng(node1.Position, node2.Position) +
                          GetReserveFromTheLeft(equipment1) + GetReserveFromTheRight(equipment2);
             }
@@ -36,7 +37,7 @@ namespace Iit.Fibertest.Graph
 
         public int CalculateDistanceBetweenNodesMm(Node leftNode, Equipment leftEquipment, Node rightNode, Equipment rightEquipment)
         {
-            var gpsDistance = (int) GpsCalculator.GetDistanceBetweenPointLatLng(leftNode.Position, rightNode.Position);
+            var gpsDistance = (int)GpsCalculator.GetDistanceBetweenPointLatLng(leftNode.Position, rightNode.Position);
 
             return (int)((gpsDistance + GetReserveFromTheLeft(leftEquipment) + GetReserveFromTheRight(rightEquipment)) * 1000);
         }
@@ -59,6 +60,36 @@ namespace Iit.Fibertest.Graph
             else if (rightEquipment.Type > EquipmentType.CableReserve)
                 rightReserve = rightEquipment.CableReserveLeft;
             return rightReserve;
+        }
+
+        public double GetFiberFullGpsDistance(Guid fiberId)
+        {
+            var fiber = _readModel.Fibers.First(f => f.Id == fiberId);
+            var node1 = _readModel.Nodes.First(n => n.Id == fiber.Node1);
+            var node2 = _readModel.Nodes.First(n => n.Id == fiber.Node2);
+            var result = GpsCalculator.GetDistanceBetweenPointLatLng(node1.Position, node2.Position);
+
+            var fId = fiberId;
+            while (node1.TypeOfLastAddedEquipment == EquipmentType.AdjustmentPoint)
+            {
+                fiber = _readModel.GetAnotherFiberOfAdjustmentPoint(node1, fId);
+                var previousNode1 = node1;
+                node1 = _readModel.Nodes.First(n => n.Id == fiber.Node1);
+                result = result + GpsCalculator.GetDistanceBetweenPointLatLng(node1.Position, previousNode1.Position);
+                fId = fiber.Id;
+            }
+
+            fId = fiberId;
+            while (node2.TypeOfLastAddedEquipment == EquipmentType.AdjustmentPoint)
+            {
+                fiber = _readModel.GetAnotherFiberOfAdjustmentPoint(node2, fId);
+                var previousNode2 = node2;
+                node2 = _readModel.Nodes.First(n => n.Id == fiber.Node2);
+                result = result + GpsCalculator.GetDistanceBetweenPointLatLng(node2.Position, previousNode2.Position);
+                fId = fiber.Id;
+            }
+
+            return result;
         }
     }
 }
--- Client/WpfClient/Forms/FiberForms/FiberUpdate/FiberUpdateViewModel.cs
+++ Client/WpfClient/Forms/FiberForms/FiberUpdate/FiberUpdateViewModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using Caliburn.Micro;
-using Iit.Fibertest.Dto;
 using Iit.Fibertest.Graph;
 using Iit.Fibertest.StringResources;
 
@@ -12,11 +11,9 @@ namespace Iit.Fibertest.Client
     public class FiberUpdateViewModel : Screen, IDataErrorInfo
     {
         private readonly ReadModel _readModel;
+        private readonly GraphGpsCalculator _graphGpsCalculator;
         private Fiber _fiber;
         private string _userInputedLength;
-
-        public string NodeAtitle { get; set; }
-        public string NodeBtitle { get; set; }
 
         public string GpsLength { get; set; }
 
@@ -37,38 +34,22 @@ namespace Iit.Fibertest.Client
         public UpdateFiber Command { get; set; }
 
        
-        public FiberUpdateViewModel(ReadModel readModel)
+        public FiberUpdateViewModel(ReadModel readModel, GraphGpsCalculator graphGpsCalculator)
         {
             _readModel = readModel;
+            _graphGpsCalculator = graphGpsCalculator;
         }
 
         public void Initialize(Guid fiberId)
         {
             _fiber = _readModel.Fibers.Single(f => f.Id == fiberId);
 
-            var n1 = GetNotAdjustmentPointEdgeOfFiber(_fiber, _readModel.Nodes.First(n=>n.Id == _fiber.Node1));
-            var n2 = GetNotAdjustmentPointEdgeOfFiber(_fiber, _readModel.Nodes.First(n=>n.Id == _fiber.Node2));
-
-            NodeAtitle = n1.Title;
-            NodeBtitle = n2.Title;
-            GpsLength = $@"{GpsCalculator.GetDistanceBetweenPointLatLng(n1.Position, n2.Position):#,##0}";
+            GpsLength = $@"{_graphGpsCalculator.GetFiberFullGpsDistance(fiberId):#,##0}";
 //            OpticalLength = _fiber.OpticalLength; // потом из базовых брать
 
             // refactor to use ReadModel instead of Graph
             UserInputedLength = _fiber.UserInputedLength.ToString(CultureInfo.InvariantCulture);
         }
-
-        private Node GetNotAdjustmentPointEdgeOfFiber(Fiber fX, Node n1)
-        {
-            while (n1.TypeOfLastAddedEquipment == EquipmentType.AdjustmentPoint)
-            {
-                fX = _readModel.GetAnotherFiberOfAdjustmentPoint(n1, fX.Id);
-                n1 = _readModel.Nodes.First(n=>n.Id == (fX.Node1 == n1.Id ? fX.Node2 : fX.Node1));
-            }
-            return n1;
-        }
-
-    
 
         protected override void OnViewLoaded(object view)
         {
