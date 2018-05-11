b5e5fa4e531ea6b437db3bb28611ffbe1b68a447
mli2805 <mli2805@gmail.com>
2018-05-11 15:26:40 +0300

    + RftsEventsViewModelSteps

---
+++ Graph.Tests/Forms/RftsEventsViewModel.feature
@@ -0,0 +1,16 @@
+﻿Feature: RftsEventsViewModel
+	
+Scenario: Форма рефлектометрических событий
+	When Открываем форму событий для измерения CriticalRnode5-MinorLnode3-MinorCnode3
+	Then Форма имеет 3 вкладки
+	Then На всех вкладках по 7 событий
+	And Состояние трассы Critical
+	And Все пороги состояние плохо
+
+	When Открываем форму событий для измерения Trace4Lm\NoFiber
+	Then Нет вкладок есть только надпись Нет волокна
+
+	When Открываем форму событий для измерения Trace5Lm\BreakBbetween2and3
+	Then Форма имеет 3 вкладки
+	And Состояние трассы fiber break
+	And Все пороги состояние плохо
---
+++ Graph.Tests/Forms/RftsEventsViewModelSteps.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Windows;
+using FluentAssertions;
+using Iit.Fibertest.IitOtdrLibrary;
+using Iit.Fibertest.WpfCommonViews;
+using TechTalk.SpecFlow;
+
+namespace Graph.Tests.Forms
+{
+    [Binding]
+    public sealed class RftsEventsViewModelSteps
+    {
+        private RftsEventsViewModel _vm;
+
+        [When(@"Открываем форму событий для измерения (.*)")]
+        public void WhenОткрываемФормуСобытийДляИзмерения(string filename)
+        {
+            var sorBytes = File.ReadAllBytes($@"..\..\Sut\MoniResults\{filename}.sor");
+            var sorData = SorData.FromBytes(sorBytes);
+            _vm = new RftsEventsViewModel(sorData);
+        }
+
+        [Then(@"Форма имеет 3 вкладки")]
+        public void ThenФормаИмеет3Вкладки()
+        {
+            _vm.RftsEventsTableVisibility.Should().Be(Visibility.Visible);
+            _vm.NoFiberLabelVisibility.Should().Be(Visibility.Collapsed);
+
+            _vm.LevelsContent.IsMinorExists.Should().BeTrue();
+            _vm.LevelsContent.IsMajorExists.Should().BeTrue();
+            _vm.LevelsContent.IsCriticalExists.Should().BeTrue();
+            _vm.LevelsContent.IsUsersExists.Should().BeFalse();
+        }
+
+        [Then(@"На всех вкладках по (.*) событий")]
+        public void ThenНаВсехВкладкахПоСобытий(int p0)
+        {
+            _vm.LevelsContent.MinorLevelViewModel.BindableTable.Columns.Count.Should().Be(p0);
+
+            _vm.LevelsContent.MinorLevelViewModel.OneLevelTableContent.IsFailed.Should().BeTrue();
+            _vm.LevelsContent.MinorLevelViewModel.OneLevelTableContent.Table[104][4].Should().Be(@" L C");
+            _vm.LevelsContent.MinorLevelViewModel.OneLevelTableContent.Table[104][6].Should().Be(@" R");
+
+            _vm.LevelsContent.MajorLevelViewModel.OneLevelTableContent.IsFailed.Should().BeTrue();
+            _vm.LevelsContent.MajorLevelViewModel.OneLevelTableContent.Table[104][6].Should().Be(@" R");
+
+            _vm.LevelsContent.CriticalLevelViewModel.OneLevelTableContent.IsFailed.Should().BeTrue();
+            _vm.LevelsContent.CriticalLevelViewModel.OneLevelTableContent.Table[104][6].Should().Be(@" R");
+        }
+
+        [Then(@"Все пороги состояние плохо")]
+        public void ThenВсеПорогиСостояниеПлохо()
+        {
+            _vm.FooterViewModel.Minor.Should().StartWith(@"fail");
+            _vm.FooterViewModel.Major.Should().StartWith(@"fail");
+            _vm.FooterViewModel.Critical.Should().StartWith(@"fail");
+        }
+
+
+        [Then(@"Состояние трассы (.*)")]
+        public void ThenСостояниеТрассы(string state)
+        {
+            _vm.FooterViewModel.TraceState.Should().StartWith(state);
+        }
+
+        [Then(@"Нет вкладок есть только надпись Нет волокна")]
+        public void ThenНетВкладокЕстьТолькоНадписьНетВолокна()
+        {
+            _vm.RftsEventsTableVisibility.Should().Be(Visibility.Collapsed);
+            _vm.NoFiberLabelVisibility.Should().Be(Visibility.Visible);
+        }
+
+    }
+}
