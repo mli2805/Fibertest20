5001cf6b29d1906f84779d11787e43f49ac2099d
mli2805 <mli2805@gmail.com>
2018-05-25 11:32:17 +0300

    - old console app dbMigrator

--- Auxiliary Files/copy-to-with-migrator.cmd
+++ Auxiliary Files/copy-to-with-migrator.cmd
@@ -1,5 +1,5 @@
 xcopy m:\client\wpfclient\bin\x86\debug\*.* c:\iit\fibertest-client\bin\*.* /S /D /Y
 xcopy m:\server\datacenterservice\bin\debug\*.* c:\iit\fibertest-datacenter\bin\*.* /S /D /Y
-xcopy m:\client\DbMigrator\bin\x86\debug\*.* c:\iit\fibertest-migrator\bin\*.* /S /D /Y
+xcopy m:\client\DbMigrationWpf\bin\debug\*.* c:\iit\fibertest-migrator\bin\*.* /S /D /Y
 
 pause
