ff67aa361c04d53408b7652bc7204f8ec9aa6f14
mli2805 <mli2805@gmail.com>
2018-06-18 13:29:54 +0300

    create shortcuts

---
+++ Auxiliary Files/copy-into-setup-folder.cmd
@@ -0,0 +1,6 @@
+xcopy c:\VSProjects\Fibertest20\Client\WpfClient\bin\Debug\*.* c:\VSProjects\Fibertest20\Setup\bin\ClientFiles\*.* /S/D/Y
+xcopy c:\VSProjects\Fibertest20\DataCenter\DataCenterService\bin\Debug\*.* c:\VSProjects\Fibertest20\Setup\bin\DcFiles\*.* /S/D/Y
+xcopy c:\VSProjects\Fibertest20\RTU\RtuService\bin\Debug\*.* c:\VSProjects\Fibertest20\Setup\bin\RtuFiles\*.* /S/D/Y
+xcopy c:\VSProjects\Fibertest20\RTU\RtuWatchdog\bin\Debug\*.* c:\VSProjects\Fibertest20\Setup\bin\RtuFiles\*.* /S/D/Y
+pause
+
--- Setup/Logic/SetupOperations.cs
+++ Setup/Logic/SetupOperations.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
+using IWshRuntimeLibrary;
 
 namespace Setup
 {
@@ -6,6 +8,9 @@ namespace Setup
     {
         private const string DataCenterServiceName = "FibertestDcService";
         private const string DataCenterDisplayName = "Fibertest 2.0 DataCenter Server";
+        private const string SourcePathClient = @"..\ClientFiles";
+        private const string TargetPathBase = @"C:\IIT-Fibertest\";
+        private readonly string _targetPathClient;
 
         private readonly CurrentInstallation _currentInstallation;
         private ObservableCollection<string> _progressLines;
@@ -13,6 +18,7 @@ namespace Setup
         public SetupOperations(CurrentInstallation currentInstallation)
         {
             _currentInstallation = currentInstallation;
+            _targetPathClient = TargetPathBase + @"Client\bin";
         }
 
         public void Run(ObservableCollection<string> progressLines)
@@ -37,6 +43,59 @@ namespace Setup
         private void SetupClient()
         {
             _progressLines.Add("Client setup started.");
+
+            _progressLines.Add("Files are copied...");
+            if (DirectoryCopy(SourcePathClient, _targetPathClient))
+                _progressLines.Add("Files are copied successfully.");
+
+            _progressLines.Add("Shortcuts are created...");
+            CreateShortcuts();
+            _progressLines.Add("Shortcuts are created successfully.");
+        }
+
+        private void CreateShortcuts()
+        {
+            object shDesktop = "Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string) shell.SpecialFolders.Item(ref shDesktop) + @"\FtClient20.lnk";
+            IWshShortcut shortcut = (IWshShortcut) shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "Fibertest 2.0 Client";
+            shortcut.TargetPath = _targetPathClient + @"\Iit.Fibertest.Client.exe";
+            shortcut.IconLocation = _targetPathClient + @"\Iit.Fibertest.Client.exe";
+            shortcut.Save();
+        }
+
+        private bool DirectoryCopy(string sourceDirName, string destDirName)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            if (!dir.Exists)
+            {
+                _progressLines.Add("Error! Source folder not found!");
+                return false;
+            }
+
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+                Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                _progressLines.Add(temppath);
+                file.CopyTo(temppath, true);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath);
+            }
+
+            return true;
         }
 
         private void SetupDataCenter()
