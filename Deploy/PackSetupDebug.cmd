4e8b2097bd9d611effbb6e236b3b2e238f173151
mli2805 <mli2805@gmail.com>
2018-06-28 16:59:35 +0300

    license xps files

--- Deploy/PackSetupDebug.cmd
+++ Deploy/PackSetupDebug.cmd
@@ -1,13 +1,21 @@
-xcopy ..\Setup\bin\Debug\*.* Deploy\Pack\bin\*.* /S/D/Y
-xcopy ..\Setup\LicenseDocs\*.xps Deploy\Pack\LicenseDocs\*.* /S/D/Y
+xcopy ..\Setup\bin\Debug\*.* Pack\bin\*.* /S/D/Y
+xcopy ..\Setup\LicenseDocs\*.xps Pack\LicenseDocs\*.* /S/D/Y
       
-xcopy ..\DataCenter\DataCenterService\bin\Debug\*.* Deploy\Pack\DcFiles\*.* /S/D/Y
-xcopy ..\Client\WpfClient\bin\Debug\*.* Deploy\Pack\ClientFiles\*.* /S/D/Y
-xcopy ..\RTU\RtuService\bin\Debug\*.* Deploy\Pack\RtuFiles\*.* /S/D/Y
-xcopy ..\RTU\RtuWatchdog\bin\Debug\*.* Deploy\Pack\RtuFiles\*.* /S/D/Y
-xcopy ..\RTU\OtdrMeasEngine\*.* Deploy\Pack\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
-pause 
-xcopy ..\Uninstall\bin\Debug\*.* Deploy\Pack\UninstallFiles\*.* /S/D/Y
+xcopy ..\DataCenter\DataCenterService\bin\Debug\*.* Pack\DcFiles\*.* /S/D/Y
+xcopy ..\Client\WpfClient\bin\Debug\*.* Pack\ClientFiles\*.* /S/D/Y
+xcopy ..\RTU\RtuService\bin\Debug\*.* Pack\RtuFiles\*.* /S/D/Y
+xcopy ..\RTU\RtuWatchdog\bin\Debug\*.* Pack\RtuFiles\*.* /S/D/Y
 
-"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" FtDebug_2.0.1.%1.exe Deploy\Pack\*.*
+mkdir Pack\RtuFiles\OtdrMeasEngine
+mkdir Pack\RtuFiles\OtdrMeasEngine\Etc
+mkdir Pack\RtuFiles\OtdrMeasEngine\Etc_default
+xcopy c:\VsGitProjects\Iit_otdr\__WinOut\Etc_default\*.* Pack\RtuFiles\OtdrMeasEngine\Etc\*.* /S/D/Y
+xcopy c:\VsGitProjects\Iit_otdr\__WinOut\Etc_default\*.* Pack\RtuFiles\OtdrMeasEngine\Etc_default\*.* /S/D/Y
+xcopy c:\VsGitProjects\Iit_otdr\__WinOut\Release_RTU\*.* Pack\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
+mkdir Pack\RtuFiles\OtdrMeasEngine\Share
+pause 
+
+xcopy ..\Uninstall\bin\Debug\*.* Pack\UninstallFiles\*.* /S/D/Y
+
+"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" FtDebug_2.0.1.%1.exe Pack\*.*
 pause
