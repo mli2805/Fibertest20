2d2ce5203e7b471d2b29e641e4b3e0a6e11a0c2e
mli2805 <mli2805@gmail.com>
2018-06-28 17:10:35 +0300

    get OtdrMeasEngine from temp folder

--- Deploy/PackSetup.cmd
+++ Deploy/PackSetup.cmd
@@ -10,7 +10,8 @@ xcopy ..\DataCenter\DataCenterService\bin\Release\*.* Pack\DcFiles\*.* /S/D/Y
 xcopy ..\Client\WpfClient\bin\Release\*.* Pack\ClientFiles\*.* /S/D/Y
 xcopy ..\RTU\RtuService\bin\Release\*.* Pack\RtuFiles\*.* /S/D/Y
 xcopy ..\RTU\RtuWatchdog\bin\Release\*.* Pack\RtuFiles\*.* /S/D/Y
-rem OtdrMeasEngine folder should be copied from another build on TeamCity
+rem OtdrMeasEngine folder will be copied from another build on TeamCity to Temp folder
+xcopy Temp\OtdrMeasEngine\*.* Pack\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
       
 xcopy ..\Uninstall\bin\Release\*.* Pack\UninstallFiles\*.* /S/D/Y
 
