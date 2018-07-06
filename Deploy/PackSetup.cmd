5b543625207a61a21257ec33fa484ef998ed5212
mli2805 <mli2805@gmail.com>
2018-07-06 11:06:44 +0300

    7z

--- Deploy/PackSetup.cmd
+++ Deploy/PackSetup.cmd
@@ -17,7 +17,8 @@ xcopy Temp\OtdrMeasEngine\*.* Pack\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
 xcopy ..\Uninstall\bin\Release\*.* Pack\UninstallFiles\*.* /S/D/Y
 
 curl --user mli:zaq1@WSX http://192.168.96.8:8888/job/RFTSViewer/lastSuccessfulBuild/artifact/trunk/Source/RftsReflect.zip --output Pack\RftsReflect.zip
-"C:\Program Files\WinRAR\rar.exe" x Pack\RftsReflect.zip
+7z.exe x Pack\RftsReflect.zip
+del Pack\RftsReflect.zip
 
 "C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" Ft_%1.exe Pack\*.*
 
