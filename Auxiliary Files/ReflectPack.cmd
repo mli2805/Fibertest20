70a872406946859f6936f321ec774fbeba3ce622
mli2805 <mli2805@gmail.com>
2018-07-06 09:57:36 +0300

    get reflect from jenkins

---
+++ Auxiliary Files/ReflectPack.cmd
@@ -0,0 +1,35 @@
+cd ..\Source\
+del RftsReflect.zip
+
+mkdir RftsReflect\
+xcopy RFTSViewer\__WinOut\reflect.exe RftsReflect\*.* /D/Y
+xcopy RFTSViewer\__WinOut\borlndmm.dll RftsReflect\*.* /D/Y
+xcopy RFTSViewer\__WinOut\cc32110.dll RftsReflect\*.* /D/Y
+xcopy RFTSViewer\__WinOut\cc32110mt.dll RftsReflect\*.* /D/Y
+xcopy RFTSViewer\_help\ReflectHelp.chm RftsReflect\*.* /D/Y
+xcopy OpticalSwitch\__WinOut\oswlaunch.exe RftsReflect\*.* /D/Y
+
+mkdir RftsReflect\etc\
+xcopy OtdrMeasEngine\__WinOut\etc_default\*.* RftsReflect\etc\*.* /D/Y
+
+mkdir RftsReflect\etc_gui\
+xcopy RFTSViewer\__WinOut\etc_gui_default\*.* RftsReflect\etc_gui\*.* /D/Y
+
+mkdir RftsReflect\lib\
+xcopy OtdrMeasEngine\__WinOut\lib\automat.dll RftsReflect\lib\*.* /D/Y
+xcopy OtdrMeasEngine\__WinOut\lib\id_com.dll RftsReflect\lib\*.* /D/Y
+xcopy OtdrMeasEngine\__WinOut\lib\id_rmtt.dll RftsReflect\lib\*.* /D/Y
+xcopy OtdrMeasEngine\__WinOut\lib\id_tcp.dll RftsReflect\lib\*.* /D/Y
+xcopy OtdrMeasEngine\__WinOut\lib\id_usbW.dll RftsReflect\lib\*.* /D/Y
+xcopy OtdrMeasEngine\__WinOut\lib\meas620.dll RftsReflect\lib\*.* /D/Y
+
+mkdir RftsReflect\lib_gui\
+xcopy RFTSViewer\__WinOut\lib_gui\sorInfoD.dll RftsReflect\lib_gui\*.* /D/Y
+
+mkdir RftsReflect\plugins\
+xcopy OpticalSwitch\__WinOut\*.dll RftsReflect\plugins\*.* /D/Y
+
+mkdir RftsReflect\share\
+
+"C:\Program Files\7-Zip\7z.exe" a -r RftsReflect.zip RftsReflect\*.* 
+
--- Deploy/PackSetup.cmd
+++ Deploy/PackSetup.cmd
@@ -16,7 +16,8 @@ xcopy Temp\OtdrMeasEngine\*.* Pack\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
       
 xcopy ..\Uninstall\bin\Release\*.* Pack\UninstallFiles\*.* /S/D/Y
 
-curl --user mli:zaq1@WSX http://192.168.96.8:8888/job/RFTSViewer/lastSuccessfulBuild/artifact/trunk/Installer/Setup.exe --output Pack\ReflectSetup.exe
+curl --user mli:zaq1@WSX http://192.168.96.8:8888/job/RFTSViewer/lastSuccessfulBuild/artifact/trunk/Installer/RftsReflect.zip --output Pack\RftsReflect.zip
+"C:\Program Files\WinRAR\rar.exe" x Pack\RftsReflect.zip
 
 "C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" Ft_%1.exe Pack\*.*
 
