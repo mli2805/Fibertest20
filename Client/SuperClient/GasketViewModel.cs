b61380144b5c364e5419a5fb898a37a61d8270cc
mli2805 <mli2805@gmail.com>
2018-07-20 14:22:31 +0300

    wpf operations moved from Shell to Gasket

--- Client/SuperClient/GasketViewModel.cs
+++ Client/SuperClient/GasketViewModel.cs
@@ -1,12 +1,40 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Controls;
 using Caliburn.Micro;
+using System.Windows.Forms.Integration;
+using Panel = System.Windows.Forms.Panel;
 
 namespace Iit.Fibertest.SuperClient
 {
     public class GasketViewModel : PropertyChangedBase
-    {   
+    {  
+        #region interop
+        private const int GwlStyle = -16;
+        private const int WsBorder = 0x00800000;
+        private const int WsChild = 0x40000000;
+        private const int SwShowmaximized = 3;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(HandleRef hWnd, HandleRef hWndParent);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+        #endregion
+
+        private Dictionary<int, int> _postfixToTabitem = new Dictionary<int, int>();
+
         public ObservableCollection<TabItem> Children { get; set; } = new ObservableCollection<TabItem>();
+
         private int _selectedTabItemIndex;
         public int SelectedTabItemIndex
         {
@@ -18,6 +46,52 @@ namespace Iit.Fibertest.SuperClient
                 NotifyOfPropertyChange();
             }
         }
-      
+
+        private Panel CreatePanel(int postfix)
+        {
+            var tabItem = new TabItem() { Header = new ContentControl() };
+            Children.Add(tabItem);
+            SelectedTabItemIndex = Children.Count -1;
+            _postfixToTabitem.Add(postfix, SelectedTabItemIndex);
+            
+            var windowsFormsHost = new WindowsFormsHost();
+            tabItem.Content = windowsFormsHost;
+
+            Panel panel = new Panel();
+            windowsFormsHost.Child = panel;
+
+            return panel;
+        }
+
+        private void PutChildOnPanel(Process childProcess, Panel panel)
+        {
+            IntPtr childHandle = childProcess.MainWindowHandle;
+            int oldStyle = GetWindowLong(childHandle, GwlStyle);
+            SetWindowLong(childHandle, GwlStyle, (oldStyle | WsChild) & ~WsBorder);
+            var parentHandle = new HandleRef(null, panel.Handle);
+            SetParent(new HandleRef(null, childHandle), parentHandle);
+            var childHandleRef = new HandleRef(null, childHandle);
+            ShowWindowAsync(childHandleRef, SwShowmaximized);
+        }
+
+        public void PutProcessOnPanel(Process childProcess, int postfix)
+        {
+            var panel = CreatePanel(postfix);
+
+            PutChildOnPanel(childProcess, panel);
+        }
+
+        public void RemoveTabItem(int postfix)
+        {
+            var tabIndex = _postfixToTabitem[postfix];
+            Children.RemoveAt(tabIndex);
+            _postfixToTabitem.Remove(postfix); 
+        }
+
+        public void BringTabItemToFront(int postfix)
+        {
+            if (_postfixToTabitem.ContainsKey(postfix))
+                SelectedTabItemIndex = _postfixToTabitem[postfix];
+        }
     }
 }
--- Client/SuperClient/ShellViewModel.cs
+++ Client/SuperClient/ShellViewModel.cs
@@ -1,7 +1,4 @@
-using System.Collections.Generic;
 using Caliburn.Micro;
-using System.Diagnostics;
-using System.Windows.Controls;
 using Iit.Fibertest.UtilsLib;
 
 namespace Iit.Fibertest.SuperClient
@@ -13,12 +10,8 @@ namespace Iit.Fibertest.SuperClient
         private readonly SuperClientWcfServiceHost _superClientWcfServiceHost;
         private ChildStarter _childStarter;
         private AddServerViewModel _addServerViewModel;
-
-        private Dictionary<int, int> _postfixToTabitem = new Dictionary<int, int>();
-        private Dictionary<int, Process> _processes = new Dictionary<int, Process>();
-
         public GasketViewModel GasketViewModel { get; set; }
-     
+
         public FtServerList FtServerList { get; set; }
         private FtServer _selectedFtServer;
         public FtServer SelectedFtServer
@@ -29,12 +22,11 @@ namespace Iit.Fibertest.SuperClient
                 if (Equals(value, _selectedFtServer)) return;
                 _selectedFtServer = value;
                 NotifyOfPropertyChange();
-                if (_postfixToTabitem.ContainsKey(_selectedFtServer.Entity.Postfix))
-                    GasketViewModel.SelectedTabItemIndex = _postfixToTabitem[_selectedFtServer.Entity.Postfix];
+                GasketViewModel.BringTabItemToFront(_selectedFtServer.Entity.Postfix);
             }
         }
 
-        public ShellViewModel(IMyLog logFile, IWindowManager windowManager, 
+        public ShellViewModel(IMyLog logFile, IWindowManager windowManager,
             SuperClientWcfServiceHost superClientWcfServiceHost, FtServerList ftServerList,
             GasketViewModel gasketViewModel,
             ChildStarter childStarter, AddServerViewModel addServerViewModel)
@@ -59,30 +51,13 @@ namespace Iit.Fibertest.SuperClient
 
         public void ConnectServer()
         {
-            var tabItem = new TabItem() { Header = new ContentControl() };
-            GasketViewModel.Children.Add(tabItem);
-            GasketViewModel.SelectedTabItemIndex = GasketViewModel.Children.Count -1;
-
-            var panel = _childStarter.CreatePanel(tabItem);
-
-            var process = _childStarter.StartChild(SelectedFtServer.Entity);
-            _processes.Add(SelectedFtServer.Entity.Postfix, process);
-
-            _childStarter.PutChildOnPanel(process, panel);
+            _childStarter.StartFtClient(SelectedFtServer.Entity);
             SelectedFtServer.ServerConnectionState = FtServerState.Connected;
-            _postfixToTabitem.Add(SelectedFtServer.Entity.Postfix, GasketViewModel.SelectedTabItemIndex);
         }
 
         public void DisconnectServer()
         {
-            var process = _processes[SelectedFtServer.Entity.Postfix];
-            process.Kill();
-            _processes.Remove(SelectedFtServer.Entity.Postfix);
-
-            var tabIndex = _postfixToTabitem[SelectedFtServer.Entity.Postfix];
-            GasketViewModel.Children.RemoveAt(tabIndex);
-            _postfixToTabitem.Remove(SelectedFtServer.Entity.Postfix);
-
+            _childStarter.CloseFtClient(SelectedFtServer.Entity);
             SelectedFtServer.ServerConnectionState = FtServerState.Disconnected;
         }
 
