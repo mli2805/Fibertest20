6dbde0aafb8f9e30542b7d08df4dc34efc396245
mli2805 <mli2805@gmail.com>
2018-08-21 09:27:35 +0300

    +Autofac in KadastrLoader

--- Client/KadastrLoader/AppBootstrapper.cs
+++ Client/KadastrLoader/AppBootstrapper.cs
@@ -1,37 +1,81 @@
-namespace KadastrLoader {
+namespace KadastrLoader 
+{
     using System;
     using System.Collections.Generic;
     using Caliburn.Micro;
+    using Autofac;
+    using System.Reflection;
+    using Iit.Fibertest.UtilsLib;
+    using System.Threading;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Markup;
 
-    public class AppBootstrapper : BootstrapperBase {
-        SimpleContainer container;
+    public class AppBootstrapper : BootstrapperBase 
+    {
+        private ILifetimeScope _container;
 
-        public AppBootstrapper() {
+        public AppBootstrapper() 
+        {
             Initialize();
         }
 
-        protected override void Configure() {
-            container = new SimpleContainer();
-
-            container.Singleton<IWindowManager, WindowManager>();
-            container.Singleton<IEventAggregator, EventAggregator>();
-            container.PerRequest<IShell, KadastrLoaderViewModel>();
+        protected override void Configure() 
+        {
         }
 
-        protected override object GetInstance(Type service, string key) {
-            return container.GetInstance(service, key);
+        protected override object GetInstance(Type service, string key) 
+        {
+            return string.IsNullOrWhiteSpace(key) ?
+                _container.Resolve(service) :
+                _container.ResolveNamed(key, service);
         }
 
-        protected override IEnumerable<object> GetAllInstances(Type service) {
-            return container.GetAllInstances(service);
+        protected override IEnumerable<object> GetAllInstances(Type service) 
+        {
+            return _container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
         }
 
-        protected override void BuildUp(object instance) {
-            container.BuildUp(instance);
+        protected override void BuildUp(object instance) 
+        {
+            _container.InjectProperties(instance);
         }
 
-        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e) {
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            SomeInitialActions();
             DisplayRootViewFor<IShell>();
+        }
+
+        private void SomeInitialActions()
+        {
+            var iniFileName = $@"kadastr.ini";
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AutofacKadastr>();
+
+            var iniFile = new IniFile();
+            iniFile.AssignFile(iniFileName);
+            builder.RegisterInstance(iniFile);
+
+            _container = builder.Build();
+
+            var currentCulture = iniFile.Read(IniSection.General, IniKey.Culture, @"ru-RU");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(currentCulture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(currentCulture);
+
+            // Ensure the current culture passed into bindings 
+            // is the OS culture. By default, WPF uses en-US 
+            // as the culture, regardless of the system settings.
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            yield return typeof(KadastrLoaderView).Assembly; // this Assembly (.exe)
         }
     }
 }
---
+++ Client/KadastrLoader/AutofacKadastr.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using Iit.Fibertest.UtilsLib;
+
+namespace KadastrLoader
+{
+    public sealed class AutofacKadastr : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<KadastrLoaderViewModel>().As<IShell>();
+            builder.RegisterType<WindowManager>().As<IWindowManager>().InstancePerLifetimeScope();
+            builder.RegisterType<LogFile>().As<IMyLog>().InstancePerLifetimeScope();
+        }
+    }
+}
