1cdac656866f9c6732bdf566d1284fdd1442a14d
mli2805 <mli2805@gmail.com>
2018-10-05 14:32:14 +0300

    file reordering and cleanup

---
+++ Client/SuperClientWcfServiceInterface/IWcfServiceInSuperClient.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Iit.Fibertest.SuperClientWcfServiceInterface
+{
+    [ServiceContract]
+    public interface IWcfServiceInSuperClient
+    {
+        [OperationContract]
+        Task<int> ClientLoadingResult(int postfix, bool isLoadedOk, bool isStateOk);
+
+        [OperationContract]
+        Task<int> NotifyConnectionBroken(int postfix);
+
+        [OperationContract]
+        Task<int> ClientClosed(int postfix);
+
+        [OperationContract]
+        Task<int> SetSystemState(int postfix, bool isStateOk);
+    }
+}
