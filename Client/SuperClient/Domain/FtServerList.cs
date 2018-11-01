52672ed84dcfddc2bce95591bc6311dc537f57e7
mli2805 <mli2805@gmail.com>
2018-11-01 15:45:57 +0300

    warning cleanup

--- Client/SuperClient/Domain/FtServerList.cs
+++ Client/SuperClient/Domain/FtServerList.cs
@@ -12,8 +12,8 @@ namespace Iit.Fibertest.SuperClient
 {
     public class FtServerList
     {
-        private byte[] _key = { 0xc5, 0x51, 0xf6, 0x4e, 0x97, 0xdc, 0xa0, 0x54, 0x89, 0x1d, 0xe6, 0x62, 0x3f, 0x27, 0x00, 0xca };
-        private byte[] _initVector = { 0xf3, 0x5e, 0x7a, 0x81, 0xae, 0x8c, 0xb4, 0x92, 0xd0, 0xf2, 0xe7, 0xc1, 0x8d, 0x54, 0x00, 0xd8 };
+        private readonly byte[] _key = { 0xc5, 0x51, 0xf6, 0x4e, 0x97, 0xdc, 0xa0, 0x54, 0x89, 0x1d, 0xe6, 0x62, 0x3f, 0x27, 0x00, 0xca };
+        private readonly byte[] _initVector = { 0xf3, 0x5e, 0x7a, 0x81, 0xae, 0x8c, 0xb4, 0x92, 0xd0, 0xf2, 0xe7, 0xc1, 0x8d, 0x54, 0x00, 0xd8 };
 
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings()
         {
@@ -84,29 +84,6 @@ namespace Iit.Fibertest.SuperClient
             }
         }
 
-        //        public void Read()
-        //        {
-        //            Servers = new ObservableCollection<FtServer>();
-        //
-        //            try
-        //            {
-        //                if (!File.Exists(_filename)) File.Create(_filename);
-        //                else
-        //                {
-        //                    var contents = File.ReadAllLines(_filename);
-        //                    foreach (var ftServerEntity in contents.Select(s => (FtServerEntity)JsonConvert.DeserializeObject(s, JsonSerializerSettings)))
-        //                    {
-        //                        Servers.Add(new FtServer(){Entity = ftServerEntity});
-        //                    }
-        //                }
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                _logFile.AppendLine($@"Servers loading: {e.Message}");
-        //            }
-        //            _logFile.AppendLine($@"{Servers.Count} server(s) in my list.");
-        //        }
-
         public void Add(FtServer ftServer)
         {
             Servers.Add(ftServer);
@@ -119,17 +96,5 @@ namespace Iit.Fibertest.SuperClient
             EncryptAndSerialize();
         }
 
-        //        private void Write()
-        //        {
-        //            try
-        //            {
-        //                var json = Servers.Select(s=>s.Entity).Select(p => JsonConvert.SerializeObject(p, JsonSerializerSettings));
-        //                File.WriteAllLines(_filename, json);
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                _logFile.AppendLine($@"Servers saving: {e.Message}");
-        //            }
-        //        }
     }
 }
