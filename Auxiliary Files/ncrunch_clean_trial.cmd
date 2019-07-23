48d8a68d70b867cfce202b63c4fa7958df449161
mli2805 <mli2805@gmail.com>
2019-07-23 09:30:23 +0300

    get last pinned build of reflect instead of successful

--- Auxiliary Files/ncrunch_clean_trial.cmd
+++ Auxiliary Files/ncrunch_clean_trial.cmd
@@ -6,6 +6,15 @@ del c:\Users\mli2805\AppData\Roaming\ncrunch_v2_2017\*.*
 del c:\Users\mli2805\AppData\Local\RM\ncrunch17*.*
 del c:\Users\mli2805\AppData\Local\Temp\ncrunch_v2_2017\*.*
 
-rem HKEY_CURRENT_USER\SOFTWARE\Remco Software\ncrunch_v2_2017
+
+del c:\Users\Leanid\AppData\Roaming\ncrunch_v2_2019\*.*
+del c:\Users\Leanid\AppData\Local\RM\ncrunch19*
+del c:\Users\Leanid\AppData\Local\Temp\ncrunch_v2_2019\*.*
+
+del c:\Users\mli2805\AppData\Roaming\ncrunch_v2_2019\*.*
+del c:\Users\mli2805\AppData\Local\RM\ncrunch19*.*
+del c:\Users\mli2805\AppData\Local\Temp\ncrunch_v2_2019\*.*
+
+rem HKEY_CURRENT_USER\SOFTWARE\Remco Software
 
 pause
