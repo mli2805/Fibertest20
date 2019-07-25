9ff20a9888fb788043aa77e3533c1dbfc7f743fc
mli2805 <mli2805@gmail.com>
2019-07-25 15:06:42 +0300

    local machine version 2.0.4.1

--- Deploy/ReleaseOnMyPC.cmd
+++ Deploy/ReleaseOnMyPC.cmd
@@ -1,5 +1,5 @@
 cd ..
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" /build Release Fibertest20.sln
+"c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" /build Release Fibertest20.sln
 pause
 cd Deploy\
 PackSetup.cmd Release_777
