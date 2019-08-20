ba2dc63994f410d39d6b6046562bad96a51dcf2e
mli2805 <mli2805@gmail.com>
2019-08-20 15:47:36 +0300

    settings request refactored

--- Shared/Http/HttpLib/D2RHttpManager.cs
+++ Shared/Http/HttpLib/D2RHttpManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ using Newtonsoft.Json;
 
 namespace HttpLib
 {
-   
+
     public class D2RHttpManager
     {
         private DoubleAddress _rtuAddress;
@@ -24,67 +25,117 @@ namespace HttpLib
         public async Task<RtuInitializedDto> GetSettings(InitializeRtuDto dto)
         {
             var result = new RtuInitializedDto();
+
+            var rootUrl = $"http://{_rtuAddress.Main.ToStringA()}/api/v1";
+            var platformResponse = await GetPlatformSettings(rootUrl, result);
+            if (!platformResponse)
+                return result;
+
+            var otdrResponse = await GetOtdrSettings(rootUrl, result);
+            if (!otdrResponse)
+                return result;
+
+            var otauResponse = await GetOtauSettings(rootUrl, result);
+            if (!otauResponse)
+                return result;
+
+            result.ReturnCode = ReturnCode.RtuInitializedSuccessfully;
             result.RtuId = dto.RtuId;
             result.RtuAddresses = dto.RtuAddresses;
             result.Maker = RtuMaker.VeEX;
-          
-            var rootUrl = $"http://{_rtuAddress.Main.ToStringA()}/api/v1";
-            var strInfo = await GetAsync($"{rootUrl}/info");
-            var info = JsonConvert.DeserializeObject<Info>(strInfo);
-            _logFile.AppendLine(strInfo);
-            result.Mfid = info.platform.name;
-            result.Mfsn = info.platform.serialNumber;
-            result.Serial = info.platform.serialNumber;
-            result.Version = info.platform.firmwareVersion;
-            result.Version2 = info.platform.moduleFirmwareVersion;
-
-            var strOtdrs = await GetAsync($"{rootUrl}/otdrs");
-            _logFile.AppendLine(strOtdrs);
-            var otdrs = JsonConvert.DeserializeObject<Otdrs>(strOtdrs);
-
-            var strOtdr = await GetAsync($"{rootUrl}/{otdrs.items[0].self}"); // could be more than one
-            _logFile.AppendLine(strOtdr);
-            var otdr = JsonConvert.DeserializeObject<Otdr>(strOtdr);
-            result.Omid = otdr.mainframeId;
-            result.Omsn = otdr.opticalModuleSerialNumber;
-            result.AcceptableMeasParams = new TreeOfAcceptableMeasParams();
-            foreach (var laserUnitPair in otdr.supportedMeasurementParameters.laserUnits)
-            {
-                var branch = new BranchOfAcceptableMeasParams();
-                foreach (var distancePair in laserUnitPair.Value.distanceRanges)
-                {
-                    var leaf = new LeafOfAcceptableMeasParams();
-                    leaf.Resolutions = distancePair.Value.resolutions;
-                    leaf.PulseDurations = distancePair.Value.pulseDurations;
-                    leaf.MeasCountsToAverage = distancePair.Value.fastAveragingTimes;
-                    leaf.PeriodsToAverage = distancePair.Value.averagingTimes;
-                    branch.Distances.Add(distancePair.Key, leaf);
-                }
-                result.AcceptableMeasParams.Units.Add(laserUnitPair.Key, branch);
-            }
-
-            var strOtaus = await GetAsync($"{rootUrl}/otaus");
-            _logFile.AppendLine(strOtaus);
-            var otaus = JsonConvert.DeserializeObject<Otaus>(strOtaus);
-
-            var strOtau = await GetAsync($"{rootUrl}/{otaus.items[0].self}"); // could be more than one
-            _logFile.AppendLine(strOtau);
-            var otau = JsonConvert.DeserializeObject<Otau>(strOtau);
-            result.Children = new Dictionary<int, OtauDto>();
-            result.Children.Add(0, new OtauDto()
-            {
-                IsOk = true,
-                OwnPortCount = otau.portCount,
-            });
-            result.OwnPortCount = otau.portCount;
-            result.FullPortCount = otau.portCount;
-
-            result.ReturnCode = ReturnCode.RtuInitializedSuccessfully;
-
-            _logFile.AppendLine(otdrs.items.Count.ToString());
-
             result.IsInitialized = true;
             return result;
+        }
+
+        private async Task<bool> GetOtauSettings(string rootUrl, RtuInitializedDto result)
+        {
+            try
+            {
+                var strOtaus = await GetAsync($"{rootUrl}/otaus");
+                _logFile.AppendLine(strOtaus);
+                var otaus = JsonConvert.DeserializeObject<Otaus>(strOtaus);
+
+                var strOtau = await GetAsync($"{rootUrl}/{otaus.items[0].self}"); // could be more than one
+                _logFile.AppendLine(strOtau);
+                var otau = JsonConvert.DeserializeObject<Otau>(strOtau);
+                result.Children = new Dictionary<int, OtauDto>();
+                result.Children.Add(0, new OtauDto()
+                {
+                    IsOk = true,
+                    OwnPortCount = otau.portCount,
+                });
+                result.OwnPortCount = otau.portCount;
+                result.FullPortCount = otau.portCount;
+                return true;
+            }
+            catch (Exception e)
+            {
+                result.ReturnCode = ReturnCode.OtauInitializationError;
+                result.ErrorMessage = e.Message;
+            }
+            return false;
+        }
+
+        private async Task<bool> GetOtdrSettings(string rootUrl, RtuInitializedDto result)
+        {
+            try
+            {
+                var strOtdrs = await GetAsync($"{rootUrl}/otdrs");
+                _logFile.AppendLine(strOtdrs);
+                var otdrs = JsonConvert.DeserializeObject<Otdrs>(strOtdrs);
+
+                var strOtdr = await GetAsync($"{rootUrl}/{otdrs.items[0].self}"); // could be more than one
+                _logFile.AppendLine(strOtdr);
+                var otdr = JsonConvert.DeserializeObject<Otdr>(strOtdr);
+                result.Omid = otdr.mainframeId;
+                result.Omsn = otdr.opticalModuleSerialNumber;
+                result.AcceptableMeasParams = new TreeOfAcceptableMeasParams();
+                foreach (var laserUnitPair in otdr.supportedMeasurementParameters.laserUnits)
+                {
+                    var branch = new BranchOfAcceptableMeasParams();
+                    foreach (var distancePair in laserUnitPair.Value.distanceRanges)
+                    {
+                        var leaf = new LeafOfAcceptableMeasParams();
+                        leaf.Resolutions = distancePair.Value.resolutions;
+                        leaf.PulseDurations = distancePair.Value.pulseDurations;
+                        leaf.MeasCountsToAverage = distancePair.Value.fastAveragingTimes;
+                        leaf.PeriodsToAverage = distancePair.Value.averagingTimes;
+                        branch.Distances.Add(distancePair.Key, leaf);
+                    }
+
+                    result.AcceptableMeasParams.Units.Add(laserUnitPair.Key, branch);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                result.ReturnCode = ReturnCode.RtuInitializationError;
+                result.ErrorMessage = e.Message;
+            }
+
+            return false;
+        }
+
+        private async Task<bool> GetPlatformSettings(string rootUrl, RtuInitializedDto result)
+        {
+            try
+            {
+                var strInfo = await GetAsync($"{rootUrl}/info");
+                var info = JsonConvert.DeserializeObject<Info>(strInfo);
+                _logFile.AppendLine(strInfo);
+                result.Mfid = info.platform.name;
+                result.Mfsn = info.platform.serialNumber;
+                result.Serial = info.platform.serialNumber;
+                result.Version = info.platform.firmwareVersion;
+                result.Version2 = info.platform.moduleFirmwareVersion;
+                return true;
+            }
+            catch (Exception e)
+            {
+                result.ReturnCode = ReturnCode.RtuInitializationError;
+                result.ErrorMessage = e.Message;
+            }
+            return false;
         }
 
         private async Task<string> GetAsync(string uri)
