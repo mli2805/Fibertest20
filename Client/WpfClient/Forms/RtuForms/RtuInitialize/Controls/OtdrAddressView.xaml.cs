f168d1e052b2c54b0fd687c2594605d5d9356004
mli2805 <mli2805@gmail.com>
2019-10-23 14:56:39 +0300

    refresh OTDR address after RTU initialization

---
+++ Client/WpfClient/Forms/RtuForms/RtuInitialize/Controls/OtdrAddressView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Iit.Fibertest.Client
+{
+    /// <summary>
+    /// Interaction logic for OtdrAddressView.xaml
+    /// </summary>
+    public partial class OtdrAddressView
+    {
+        public OtdrAddressView()
+        {
+            InitializeComponent();
+        }
+    }
+}
