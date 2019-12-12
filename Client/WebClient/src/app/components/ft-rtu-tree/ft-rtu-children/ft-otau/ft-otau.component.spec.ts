a0c9b32ab422fa42c6446be0e3cc3b0fe642b4a4
mli2805 <mli2805@gmail.com>
2019-12-12 12:20:14 +0300

    all tree components have the same height and hover color

--- Client/WebClient/src/app/components/ft-rtu-tree/ft-rtu-children/ft-otau/ft-otau.component.spec.ts
+++ Client/WebClient/src/app/components/ft-rtu-tree/ft-rtu-children/ft-otau/ft-otau.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { FtOtauComponent } from './ft-otau.component';
+import { FtOtauComponent } from "./ft-otau.component";
 
-describe('FtOtauV2Component', () => {
+describe("FtOtauV2Component", () => {
   let component: FtOtauComponent;
   let fixture: ComponentFixture<FtOtauComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FtOtauComponent ]
-    })
-    .compileComponents();
+      declarations: [FtOtauComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('FtOtauV2Component', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
