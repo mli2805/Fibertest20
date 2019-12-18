8ef8dbf7257461d415d6bab47d08da73e54ed55a
mli2805 <mli2805@gmail.com>
2019-12-18 16:09:51 +0300

    little renaming

--- Client/WebClient/src/app/components/details/rtu/ft-rtu-experiment/ft-rtu-experiment.component.ts
+++ Client/WebClient/src/app/components/details/rtu/ft-rtu-experiment/ft-rtu-experiment.component.ts
@@ -8,7 +8,6 @@ import { RtuMonitoringSettingsDto } from "src/app/models/dtos/rtu/rtuMonitoringS
 
 export interface PortLine {
   portMonitoringMode: PortMonitoringMode;
-  position: number;
   port: string;
   disabled: boolean;
   traceTitle: string;
@@ -23,11 +22,11 @@ export interface PortLine {
   styleUrls: ["./ft-rtu-experiment.component.css"]
 })
 export class FtRtuExperimentComponent implements OnInit {
-  ELEMENT_DATA2: PortLine[];
+  tableData: PortLine[];
 
-  displayedColumns2: string[] = ["select", "port", "traceTitle", "duration"];
-  dataSource2 = new MatTableDataSource<PortLine>();
-  selection2 = new SelectionModel<PortLine>(true, []);
+  displayedColumns: string[] = ["select", "port", "traceTitle", "duration"];
+  dataSource = new MatTableDataSource<PortLine>();
+  selectionModel = new SelectionModel<PortLine>(true, []);
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -41,25 +40,25 @@ export class FtRtuExperimentComponent implements OnInit {
       .subscribe((res: RtuMonitoringSettingsDto) => {
         console.log("rtu monitoring settings received");
         this.createPortLines(res);
-        this.dataSource2 = new MatTableDataSource<PortLine>(this.ELEMENT_DATA2);
-        this.selection2 = new SelectionModel<PortLine>(true, []);
+        this.dataSource = new MatTableDataSource<PortLine>(this.tableData);
+        this.selectionModel = new SelectionModel<PortLine>(true, []);
       });
   }
 
-  /** Whether the number of selected elements matches the total number of rows. */
+  /** Whether the number of selected elements matches the total number of enabled rows. */
   isAllSelected() {
-    const numSelected = this.selection2.selected.length;
-    const numRows = this.ELEMENT_DATA2.filter(l => !l.disabled).length;
+    const numSelected = this.selectionModel.selected.length;
+    const numRows = this.tableData.filter(l => !l.disabled).length;
     return numSelected === numRows;
   }
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected()
-      ? this.selection2.clear()
-      : this.dataSource2.data.forEach(row => {
+      ? this.selectionModel.clear()
+      : this.dataSource.data.forEach(row => {
           if (!row.disabled) {
-            this.selection2.select(row);
+            this.selectionModel.select(row);
           }
         });
   }
@@ -68,14 +67,13 @@ export class FtRtuExperimentComponent implements OnInit {
     if (row.disabled) {
       return;
     }
-    this.selection2.toggle(row);
+    this.selectionModel.toggle(row);
   }
 
   private createPortLines(res: RtuMonitoringSettingsDto) {
-    this.ELEMENT_DATA2 = res.lines.map(l => {
+    this.tableData = res.lines.map(l => {
       return {
         portMonitoringMode: l.portMonitoringMode,
-        position: 1,
         disabled:
           l.portMonitoringMode === PortMonitoringMode.NoTraceJoined ||
           l.portMonitoringMode === PortMonitoringMode.TraceHasNoBase,
