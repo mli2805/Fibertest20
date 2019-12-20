5599ad95dc330a37e6c26ef896a4eb0af708e4b6
mli2805 <mli2805@gmail.com>
2019-12-20 10:25:55 +0300

    monitoring settings view responsive design

--- Client/WebClient/src/app/components/details/rtu/ft-rtu-experiment/ft-rtu-experiment.component.ts
+++ Client/WebClient/src/app/components/details/rtu/ft-rtu-experiment/ft-rtu-experiment.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { SelectionModel } from "@angular/cdk/collections";
-import { MatTableDataSource } from "@angular/material/table";
-import { PortMonitoringMode } from "src/app/models/enums/portMonitoringMode";
-import { RtuApiService } from "src/app/api/rtu.service";
+import {
+  RtuMonitoringSettingsDto,
+  RtuMonitoringPortDto
+} from "src/app/models/dtos/rtu/rtuMonitoringSettingsDto";
 import { ActivatedRoute } from "@angular/router";
-import { RtuMonitoringSettingsDto } from "src/app/models/dtos/rtu/rtuMonitoringSettingsDto";
+import { RtuApiService } from "src/app/api/rtu.service";
+import { Frequency } from "src/app/models/enums/frequency";
+import { FrequencyPipe } from "src/app/pipes/frequency.pipe";
+import { SelectionModel } from "@angular/cdk/collections";
+import { PortMonitoringMode } from "src/app/models/enums/portMonitoringMode";
+import { MatTableDataSource } from "@angular/material/table";
+import { MonitoringMode } from "src/app/models/enums/monitoringMode";
 
 export interface PortLine {
   portMonitoringMode: PortMonitoringMode;
@@ -22,26 +28,57 @@ export interface PortLine {
   styleUrls: ["./ft-rtu-experiment.component.css"]
 })
 export class FtRtuExperimentComponent implements OnInit {
+  vm: RtuMonitoringSettingsDto = new RtuMonitoringSettingsDto();
+  itemsSource;
+  oneItemSource;
+  selectedPreciseMeas;
+  selectedPreciseSave;
+  selectedFastMeas;
+  selectedFastSave;
+
   tableData: PortLine[];
 
   displayedColumns: string[] = ["select", "port", "traceTitle", "duration"];
   dataSource = new MatTableDataSource<PortLine>();
   selectionModel = new SelectionModel<PortLine>(true, []);
 
+  cycleTime;
+  monitoringMode;
+
   constructor(
     private activeRoute: ActivatedRoute,
-    private rtuApiService: RtuApiService
+    private rtuApiService: RtuApiService,
+    private frequencyPipe: FrequencyPipe
   ) {}
 
   ngOnInit() {
+    const frs = Object.keys(Frequency)
+      .filter(e => !isNaN(+e))
+      .map(e => {
+        return { index: +e, name: this.frequencyPipe.transform(+e) };
+      });
+    this.itemsSource = frs;
+
     const id = this.activeRoute.snapshot.paramMap.get("id");
     this.rtuApiService
       .getOneRtu(id, "monitoring-settings")
       .subscribe((res: RtuMonitoringSettingsDto) => {
         console.log("rtu monitoring settings received");
-        this.createPortLines(res);
+        this.vm = res;
+        this.selectedPreciseMeas = res.preciseMeas;
+        this.selectedPreciseSave = res.preciseSave;
+        this.selectedFastMeas = "pp";
+        this.selectedFastSave = res.fastSave;
+        this.createPortLines();
         this.dataSource = new MatTableDataSource<PortLine>(this.tableData);
-        this.selectionModel = new SelectionModel<PortLine>(true, []);
+        this.selectionModel = new SelectionModel<PortLine>(
+          true,
+          this.tableData.filter(
+            l => l.portMonitoringMode === PortMonitoringMode.On
+          )
+        );
+        this.cycleTime = this.evaluateCycleTime();
+        this.monitoringMode = res.monitoringMode === MonitoringMode.On ? 0 : 1;
       });
   }
 
@@ -61,6 +98,7 @@ export class FtRtuExperimentComponent implements OnInit {
             this.selectionModel.select(row);
           }
         });
+    this.cycleTime = this.evaluateCycleTime();
   }
 
   slaveToggle(row: PortLine) {
@@ -68,10 +106,22 @@ export class FtRtuExperimentComponent implements OnInit {
       return;
     }
     this.selectionModel.toggle(row);
+    this.cycleTime = this.evaluateCycleTime();
   }
 
-  private createPortLines(res: RtuMonitoringSettingsDto) {
-    this.tableData = res.lines.map(l => {
+  private evaluateCycleTime(): number {
+    if (this.selectionModel.selected.length < 1) {
+      return 0;
+    }
+    const baseRefSum = this.selectionModel.selected.reduce(
+      (a, p) => a + p.durationOfFastBase,
+      0
+    );
+    return baseRefSum + this.selectionModel.selected.length * 2;
+  }
+
+  private createPortLines() {
+    this.tableData = this.vm.lines.map(l => {
       return {
         portMonitoringMode: l.portMonitoringMode,
         disabled:
