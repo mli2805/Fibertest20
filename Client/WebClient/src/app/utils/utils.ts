a34983e0497e5d8b42c9e818d85e83242875fad2
mli2805 <mli2805@gmail.com>
2019-12-24 14:43:02 +0300

    port changed

--- Client/WebClient/src/app/utils/utils.ts
+++ Client/WebClient/src/app/utils/utils.ts
@@ -1,10 +1,8 @@
 export class Utils {
   static GetWebApiUrl(): string {
     const protocol = "http://";
-    const port = 11837;
+    const port = 11080;
     // const port = 44362;
     return protocol + window.location.hostname + ":" + port;
   }
-  
-  
 }
