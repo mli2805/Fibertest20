a8cdd85646018bac285beb15b28314350e7f13c2
mli2805 <mli2805@gmail.com>
2020-03-09 14:28:29 +0300

    https experiment

--- Client/WebClient/src/app/utils/utils.ts
+++ Client/WebClient/src/app/utils/utils.ts
@@ -1,8 +1,8 @@
 export class Utils {
   static GetWebApiUrl(): string {
-    const protocol = "http://";
-    const port = 11080;
-    // const port = 44362;
+    const protocol = "https://";
+    // const port = 11080;
+    const port = 44371;
     return protocol + window.location.hostname + ":" + port;
   }
 }
--- DataCenter/DataCenterWebApi/Program.cs
+++ DataCenter/DataCenterWebApi/Program.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Iit.Fibertest.DataCenterWebApi
@@ -10,14 +13,24 @@ namespace Iit.Fibertest.DataCenterWebApi
             CreateHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // just as example
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            var logLevel = configuration["Logging:LogLevel:Default"];
+            Console.Write(logLevel);
+
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:11080");
+                    webBuilder.UseUrls("https://*:44371");
                 });
-
-      
+        }
     }
 }
