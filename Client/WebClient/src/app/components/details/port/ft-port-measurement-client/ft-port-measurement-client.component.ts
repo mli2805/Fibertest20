17f2efd3ce05b5dc60c9fc7bf847d6ad998404f9
mli2805 <mli2805@gmail.com>
2020-04-15 18:37:11 +0300

    measurement-client pretty face

--- Client/WebClient/src/app/components/details/port/ft-port-measurement-client/ft-port-measurement-client.component.ts
+++ Client/WebClient/src/app/components/details/port/ft-port-measurement-client/ft-port-measurement-client.component.ts
@@ -9,6 +9,21 @@ import { TreeOfAcceptableVeasParams } from "src/app/models/dtos/meas-params/acce
   styleUrls: ["./ft-port-measurement-client.component.css"],
 })
 export class FtPortMeasurementClientComponent implements OnInit {
+  vm: TreeOfAcceptableVeasParams;
+
+  itemsSourceUnits;
+  itemsSourceDistances;
+  itemsSourceResolutions;
+  itemsSourcePulseDurations;
+  itemsSourcePeriodsToAverages;
+  selectedUnit;
+  selectedDistance;
+  selectedResolution;
+  selectedPulseDuration;
+  selectedPeriodToAverage;
+  selectedBc = 0;
+  selectedRi = 0;
+
   constructor(
     private dataStorage: FtDetachedTracesProvider,
     private rtuApiService: RtuApiService
@@ -16,11 +31,42 @@ export class FtPortMeasurementClientComponent implements OnInit {
 
   ngOnInit() {
     const trace = this.dataStorage.data["trace"];
-    console.log(trace);
     this.rtuApiService
       .getOneRtu(trace.rtuId, "measurement-parameters")
       .subscribe((res: TreeOfAcceptableVeasParams) => {
-        console.log(res);
+        console.log("tree: ", res);
+        this.vm = res;
+        this.initializeLists();
       });
   }
+
+  /* tslint:disable:no-string-literal */
+  initializeLists() {
+    const tree = this.vm["units"];
+
+    const units = Object.keys(tree);
+    this.itemsSourceUnits = units;
+    this.selectedUnit = units[0];
+
+    const selectedUnitBranch = tree[this.selectedUnit];
+    this.selectedBc = selectedUnitBranch["backscatteredCoefficient"];
+    this.selectedRi = selectedUnitBranch["refractiveIndex"];
+    const distances = selectedUnitBranch["distances"];
+
+    const distancesKeys = Object.keys(distances);
+    this.itemsSourceDistances = distancesKeys;
+    this.selectedDistance = distancesKeys[0];
+
+    const leaf = distances[this.selectedDistance];
+
+    this.itemsSourceResolutions = leaf["resolutions"];
+    this.selectedResolution = leaf["resolutions"][0];
+
+    this.itemsSourcePulseDurations = leaf["pulseDurations"];
+    this.selectedPulseDuration = leaf["pulseDurations"][0];
+
+    this.itemsSourcePeriodsToAverages = leaf["periodsToAverage"];
+    this.selectedPeriodToAverage = leaf["periodsToAverage"][0];
+  }
+  /* tslint:enable:no-string-literal */
 }
