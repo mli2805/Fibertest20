37d7d1c1b71ef1c238d4565b7f8b1a8255a85f96
mli2805 <mli2805@gmail.com>
2020-04-22 20:20:51 +0300

    misc api service unified

--- Client/WebClient/src/app/api/one.service.ts
+++ Client/WebClient/src/app/api/one.service.ts
@@ -8,6 +8,17 @@ import { Utils } from "../Utils/utils";
 export class OneApiService {
   constructor(private httpClient: HttpClient) {}
 
+  getRequest(request: string) {
+    const url = Utils.GetWebApiUrl() + `/${request}`;
+    const currentUser = JSON.parse(sessionStorage.currentUser);
+
+    const myHeaders = new HttpHeaders({
+      Authorization: "Bearer " + currentUser.jsonWebToken,
+    });
+
+    return this.httpClient.get(url, { headers: myHeaders });
+  }
+
   postRequest(controller: string, request: string, body: any) {
     const url = Utils.GetWebApiUrl() + `/${controller}/${request}`;
     const currentUser = JSON.parse(sessionStorage.currentUser);
--- Client/WebClient/src/app/components/ft-about/ft-about.component.ts
+++ Client/WebClient/src/app/components/ft-about/ft-about.component.ts
@@ -1,38 +1,28 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { RtuDto } from "src/app/models/dtos/rtuTree/rtuDto";
-import { RtuApiService } from "src/app/api/rtu.service";
 import { UserDto } from "src/app/models/dtos/userDto";
 import { Role } from "src/app/models/enums/role";
-import { MiscService } from "src/app/api/misc.service";
 import { AboutDto, AboutRtuDto } from "src/app/models/dtos/aboutDto";
+import { OneApiService } from "src/app/api/one.service";
 
 @Component({
   selector: "ft-about",
   templateUrl: "./ft-about.component.html",
-  styleUrls: ["./ft-about.component.css"]
+  styleUrls: ["./ft-about.component.css"],
 })
 export class FtAboutComponent implements OnInit {
   loggedUser: UserDto;
-  aboutVm: AboutDto;
+  aboutVm: AboutDto = new AboutDto();
   rtus: AboutRtuDto[];
   roleString: string;
   columnsToDisplay = ["title", "version", "version2"];
 
-  constructor(
-    private rtuService: RtuApiService,
-    private miscService: MiscService
-  ) {}
+  constructor(private oneApiService: OneApiService) {}
 
   ngOnInit() {
     this.loggedUser = JSON.parse(sessionStorage.getItem("currentUser"));
     this.roleString = Role[this.loggedUser.role];
 
-    this.aboutVm = new AboutDto();
-    this.aboutVm.dcSoftware = "asdf";
-    this.aboutVm.webApiSoftware = "acbsdtw";
-
-    this.miscService.getAbout().subscribe((res: AboutDto) => {
-      console.log("about view model received");
+    this.oneApiService.getRequest("misc/about").subscribe((res: AboutDto) => {
       this.aboutVm = res;
     });
   }
