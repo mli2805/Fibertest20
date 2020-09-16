a185dc6a4312ced980b1212d7138ea0cc86c8b6c
mli2805 <mli2805@gmail.com>
2020-09-16 08:59:58 +0300

    show monitoring result for detached trace

--- Client/Graph/Algorithms/AccidentsOnTraceToModelApplier.cs
+++ Client/Graph/Algorithms/AccidentsOnTraceToModelApplier.cs
@@ -35,7 +35,7 @@ namespace Iit.Fibertest.Graph
 
             model.CleanAccidentPlacesOnTrace(trace.TraceId);
 
-            if (e.TraceState != FiberState.Ok && e.TraceState != FiberState.NoFiber)
+            if (e.TraceState != FiberState.Ok && e.TraceState != FiberState.NoFiber && e.TraceState != FiberState.Unknown)
                 e.Accidents.ForEach(a => model.ShowAccidentPlaceOnTrace(a, e.TraceId));
         }
 
