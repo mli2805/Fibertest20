cf1b80e84655522a998bef24860bc9960ce50d98
mli2805 <mli2805@gmail.com>
2021-02-25 10:05:53 +0300

    trace state on fast measurement is always Suspicion

--- Client/WebClient/src/app/pipes/fiber-state.pipe.ts
+++ Client/WebClient/src/app/pipes/fiber-state.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { BaseRefType } from "../models/enums/baseRefType";
 import { FiberState } from "../models/enums/fiberState";
 
 @Pipe({
@@ -8,38 +9,47 @@ import { FiberState } from "../models/enums/fiberState";
 export class FiberStatePipe implements PipeTransform {
   constructor(private ts: TranslateService) {}
 
-  transform(value: FiberState): string {
-    switch (value) {
-      case FiberState.Nothing:
-        return "";
-      case FiberState.NotInTrace:
-        return this.ts.instant("SID_Not_in_trace");
-      case FiberState.NotJoined:
-        return this.ts.instant("SID_Not_joined");
-      case FiberState.DistanceMeasurement:
-        return this.ts.instant("SID_Distace_measurement");
-      case FiberState.Unknown:
-        return this.ts.instant("SID_Unknown");
-      case FiberState.Ok:
-        return this.ts.instant("SID_Ok");
-      case FiberState.Suspicion:
+  transform(value: FiberState, arg1: BaseRefType): string {
+    if (value <= FiberState.Ok) {
+      switch (value) {
+        case FiberState.Nothing:
+          return "";
+        case FiberState.NotInTrace:
+          return this.ts.instant("SID_Not_in_trace");
+        case FiberState.NotJoined:
+          return this.ts.instant("SID_Not_joined");
+        case FiberState.DistanceMeasurement:
+          return this.ts.instant("SID_Distace_measurement");
+        case FiberState.Unknown:
+          return this.ts.instant("SID_Unknown");
+        case FiberState.Ok:
+          return this.ts.instant("SID_Ok");
+        default:
+          return this.ts.instant("SID_Ok");
+      }
+    } else {
+      if (arg1 === BaseRefType.Fast) {
         return this.ts.instant("SID_Suspicion");
-      case FiberState.Minor:
-        return this.ts.instant("SID_Minor");
-      case FiberState.Major:
-        return this.ts.instant("SID_Major");
-      case FiberState.User:
-        return this.ts.instant("SID_User_s_threshold");
-      case FiberState.Critical:
-        return this.ts.instant("SID_Critical");
-      case FiberState.FiberBreak:
-        return this.ts.instant("SID_fiber_break");
-      case FiberState.NoFiber:
-        return this.ts.instant("SID_No_fiber");
-      case FiberState.HighLighted:
-        return this.ts.instant("SID_Highlighted");
-      default:
-        return this.ts.instant("SID_Ok");
+      } else {
+        switch (value) {
+          case FiberState.Minor:
+            return this.ts.instant("SID_Minor");
+          case FiberState.Major:
+            return this.ts.instant("SID_Major");
+          case FiberState.User:
+            return this.ts.instant("SID_User_s_threshold");
+          case FiberState.Critical:
+            return this.ts.instant("SID_Critical");
+          case FiberState.FiberBreak:
+            return this.ts.instant("SID_fiber_break");
+          case FiberState.NoFiber:
+            return this.ts.instant("SID_No_fiber");
+          case FiberState.HighLighted:
+            return this.ts.instant("SID_Highlighted");
+          default:
+            return this.ts.instant("SID_Ok");
+        }
+      }
     }
   }
 }
