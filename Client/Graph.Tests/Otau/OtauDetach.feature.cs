0712c97cde5bf183b2393eae50b7fd8ceb7fc38a
mli2805 <mli2805@gmail.com>
2021-10-08 11:56:24 +0300

    just file reordering

--- Client/Graph.Tests/Otau/OtauDetach.feature.cs
+++ Client/Graph.Tests/Otau/OtauDetach.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Graph.Tests.Rtu
+namespace Graph.Tests.Otau
 {
     using TechTalk.SpecFlow;
     
@@ -149,3 +149,4 @@ this.FeatureBackground();
 }
 #pragma warning restore
 #endregion
+
