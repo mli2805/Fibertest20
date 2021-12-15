274c8a2f7011edcc35fcdd5c01fa3a64c36f0122
mli2805 <mli2805@gmail.com>
2021-12-15 09:05:35 +0300

    GetTraceDetalization creates new fiber instead of eliminated

--- Client/Graph/DomainEntities/TraceDetalization.cs
+++ Client/Graph/DomainEntities/TraceDetalization.cs
@@ -6,6 +6,6 @@ namespace Iit.Fibertest.Graph
     public class TraceDetalization
     {
         public List<Guid> Nodes { get; set; }
-        public List<Guid> Fibers { get; set; }
+        public List<Fiber> Fibers { get; set; }
     }
 }
--- Client/Graph/EventSourcing/Model/TraceExt.cs
+++ Client/Graph/EventSourcing/Model/TraceExt.cs
@@ -13,25 +13,40 @@ namespace Iit.Fibertest.Graph
                 .Select(i => readModel.Equipments.Single(eq => eq.EquipmentId == i)).ToList();
 
             var nodes = new List<Guid>() { trace.NodeIds[0] }; // RTU
-            var fibers = new List<Guid>();
+            var fibers = new List<Fiber>();
             var previousNode = 0;
 
-            for (int i = 0; i < tEquips.Count; i++)
+            for (int i = 1; i <= tEquips.Count; i++)
             {
-                if (tEquips[i].Type >= upToType || i == tEquips.Count - 1 && upToType == EquipmentType.RtuAndEot)
+                if (tEquips[i-1].Type >= upToType || i == tEquips.Count && upToType == EquipmentType.RtuAndEot)
                 {
                     nodes.Add(trace.NodeIds[i]);
                     if (i - previousNode == 1)
                     {
-                        fibers.Add(trace.FiberIds[i-1]);
+                        var fiber = readModel.Fibers.First(f => f.FiberId == trace.FiberIds[i - 1]);
+                        fibers.Add(fiber);
                     }
                     else
                     {
-                        fibers.Add(Guid.NewGuid());
+                        var fiber = new Fiber() 
+                            {FiberId = Guid.NewGuid(), NodeId1 = nodes[nodes.Count-2], NodeId2 = nodes[nodes.Count-1]};
+                        for (int j = previousNode; j < i; j++)
+                        {
+                            var fiber1 = readModel.Fibers.First(f => f.FiberId == trace.FiberIds[j]);
+                            foreach (var fiber1State in fiber1.States)
+                            {
+                                if (!fiber.States.ContainsKey(fiber1State.Key))
+                                    fiber.States.Add(fiber1State.Key, fiber1State.Value);
+                                else if (fiber1State.Value > fiber.States[fiber1State.Key])
+                                    fiber.States[fiber1State.Key] = fiber1State.Value;
+                            }
+                        }
+
+                        fibers.Add(fiber);
                     }
+                    previousNode = i;
                 }
             }
-
 
             return new TraceDetalization() { Nodes = nodes, Fibers = fibers };
         }
