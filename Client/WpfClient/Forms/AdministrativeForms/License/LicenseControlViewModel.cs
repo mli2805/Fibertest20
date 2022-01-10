f93c4f47b9672ef09f349bb937aac27d28512b04
mli2805 <mli2805@gmail.com>
2022-01-10 16:25:19 +0300

    fixed license control view

--- Client/WpfClient/Forms/AdministrativeForms/License/LicenseControlViewModel.cs
+++ Client/WpfClient/Forms/AdministrativeForms/License/LicenseControlViewModel.cs
@@ -20,26 +20,5 @@ namespace Iit.Fibertest.Client
 
         public bool IsBasic => !License.IsIncremental;
         public bool IsStandart => !License.IsMachineKeyRequired;
-
-        public void FromFile(LicenseInFile licenseInFile)
-        {
-            License = Map(licenseInFile);
-        }
-
-        private License Map(LicenseInFile licenseInFile)
-        {
-            return new License
-            {
-                Owner = licenseInFile.Owner,
-                IsIncremental = licenseInFile.IsIncremental,
-                RtuCount = new LicenseParameter(licenseInFile.RtuCount),
-                ClientStationCount = new LicenseParameter(licenseInFile.ClientStationCount),
-                WebClientCount = new LicenseParameter(licenseInFile.WebClientCount),
-                SuperClientStationCount = new LicenseParameter(licenseInFile.SuperClientStationCount),
-                IsMachineKeyRequired = licenseInFile.IsMachineKeyRequired,
-                CreationDate = licenseInFile.CreationDate,
-                Version = licenseInFile.Version,
-            };
-        }
     }
 }
