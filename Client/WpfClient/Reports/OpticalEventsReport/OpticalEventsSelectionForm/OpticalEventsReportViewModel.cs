8d6a6d612948512ed0332bd1794f0777ee2a8bef
mli2805 <mli2805@gmail.com>
2022-02-14 14:59:17 +0300

    save optical events report period while client started

--- Client/WpfClient/Reports/OpticalEventsReport/OpticalEventsSelectionForm/OpticalEventsReportViewModel.cs
+++ Client/WpfClient/Reports/OpticalEventsReport/OpticalEventsSelectionForm/OpticalEventsReportViewModel.cs
@@ -28,6 +28,9 @@ namespace Iit.Fibertest.Client
             _allOpticalEventsReportProvider = allOpticalEventsReportProvider;
 
             Model.IsCustomReport = true;
+
+            Model.DateTo = DateTime.Now;
+            Model.DateFrom = new DateTime(DateTime.Today.Year, 1, 1);
         }
 
         public void Initialize()
@@ -38,8 +41,8 @@ namespace Iit.Fibertest.Client
                 : new List<Zone>() {_readModel.Zones.First(z => z.ZoneId == _currentUser.ZoneId)};
             Model.SelectedZone = Model.Zones.First();
 
-            Model.DateTo = DateTime.Now;
-            Model.DateFrom = new DateTime(DateTime.Today.Year, 1, 1);
+            // Model.DateTo = DateTime.Now;
+            // Model.DateFrom = new DateTime(DateTime.Today.Year, 1, 1);
         }
 
         protected override void OnViewLoaded(object view)
