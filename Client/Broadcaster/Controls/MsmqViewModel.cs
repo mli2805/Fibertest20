1ab29c91a7dcd157cac1ccfe28e1ff609515bbe7
mli2805 <mli2805@gmail.com>
2022-06-15 16:44:32 +0300

    msmq sender changed to send ok and broken results separately

--- Client/Broadcaster/Controls/MsmqViewModel.cs
+++ Client/Broadcaster/Controls/MsmqViewModel.cs
@@ -14,7 +14,7 @@ namespace Broadcaster
     {
         private readonly IniFile _iniFile;
 
-        public string ServerIp { get; set; } = "192.168.96.21";
+        public string ServerIp { get; set; } = "192.168.97.14";
         public string SorFileName { get; set; } = @"..\file.sor";
         public string SorBreakFileName { get; set; } = @"..\fileBreak.sor";
         public Guid RtuId;
@@ -39,12 +39,24 @@ namespace Broadcaster
             _iniFile = iniFile;
         }
 
-        public void StartSending()
+        bool flag = false;
+        public void StartSendingOk()
         {
             var s = _iniFile.Read(IniSection.Broadcast, IniKey.MsmqTestRtuId, "00bf8f28-345f-44dc-af18-1ba6d9e4c563");
             RtuId = Guid.Parse(s);
             s = _iniFile.Read(IniSection.Broadcast, IniKey.MsmqTestTraceId, "d7f1f9ab-23bc-418d-82e6-ff755fc2b469");
             TraceId = Guid.Parse(s);
+            flag = true;
+            Task.Factory.StartNew(VeryLongOperation);
+        }
+
+        public void StartSendingBroken()
+        {
+            var s = _iniFile.Read(IniSection.Broadcast, IniKey.MsmqTestRtuId, "00bf8f28-345f-44dc-af18-1ba6d9e4c563");
+            RtuId = Guid.Parse(s);
+            s = _iniFile.Read(IniSection.Broadcast, IniKey.MsmqTestTraceId, "d7f1f9ab-23bc-418d-82e6-ff755fc2b469");
+            TraceId = Guid.Parse(s);
+            flag = false;
             Task.Factory.StartNew(VeryLongOperation);
         }
 
@@ -56,15 +68,25 @@ namespace Broadcaster
 
             var connectionString = $@"FormatName:DIRECT=TCP:{ServerIp}\private$\Fibertest20";
             var queue = new MessageQueue(connectionString);
-
-            System.Messaging.Message message = new System.Messaging.Message(dto, new BinaryMessageFormatter());
-            System.Messaging.Message messageBroken = new System.Messaging.Message(dtoBroken, new BinaryMessageFormatter());
+            System.Messaging.Message message;
+            if (flag)
+            {
+                message = new System.Messaging.Message(dto, new BinaryMessageFormatter());
+            }
+            else
+            {
+                message = new System.Messaging.Message(dtoBroken, new BinaryMessageFormatter());
+            }
+            // System.Messaging.Message messageBroken = new System.Messaging.Message(dtoBroken, new BinaryMessageFormatter());
             IncreaseSentCounter del = F;
             for (int i = 0; i < MsmqCount; i++)
             {
-                queue.Send(i % 4 == 0 ? messageBroken : message, MessageQueueTransactionType.Single);
+                // queue.Send(i % 4 == 0 ? messageBroken : message, MessageQueueTransactionType.Single);
+                queue.Send(message, MessageQueueTransactionType.Single);
                 Application.Current.Dispatcher?.Invoke(del);
                 Thread.Sleep(MsmqPauseMs);
+
+               
             }
         }
 
