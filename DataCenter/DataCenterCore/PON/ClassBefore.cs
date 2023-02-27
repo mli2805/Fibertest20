c5e6c8862a2c3c8f09a8112a72676a41867e87f0
mli2805 <mli2805@gmail.com>
2023-02-27 20:07:50 +0300

    + ClassBefore

---
+++ DataCenter/DataCenterCore/PON/ClassBefore.cs
@@ -0,0 +1,11 @@
+﻿namespace Iit.Fibertest.DataCenterCore
+{
+    public class ClassBefore
+    {
+        public int  Id { get; set; }
+        public ClassBefore()
+        {
+            Id = 0;
+        }
+    }
+}
