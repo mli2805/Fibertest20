3f388aa7dd319381d11ea90cf89ab7d09c4886c5
mli2805 <mli2805@gmail.com>
2023-03-17 15:04:19 +0300

    fixed await - Wait bug in linq foreach

--- Client/WpfClient/EventSourcing/ClientPoller.cs
+++ Client/WpfClient/EventSourcing/ClientPoller.cs
@@ -190,7 +190,7 @@ namespace Iit.Fibertest.Client
                     _rtuStateViewsManager.Apply(evnt);
                     _traceStateViewsManager.Apply(evnt);
                     _traceStatisticsViewsManager.Apply(evnt);
-                    _landmarksViewsManager.Apply(evnt);
+                    await _landmarksViewsManager.Apply(evnt);
                     _rtuChannelViewsManager.Apply(evnt);
                     _bopStateViewsManager.Apply(evnt);
                     _bopNetworkEventsDoubleViewModel.Apply(evnt);
