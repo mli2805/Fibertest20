b90322c31ae6a06e54fe69b0e62b7e2a01f79451
mli2805 <mli2805@gmail.com>
2023-07-11 14:18:01 +0300

    Web: fixed missed fields

--- Client/WebClient/src/app/components/ft-state-accidents/ft-state-accidents.component.ts
+++ Client/WebClient/src/app/components/ft-state-accidents/ft-state-accidents.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
-import { MatPaginator, MatSort, MatToolbar, MatMenuTrigger } from '@angular/material';
-import { MatSlideToggleModule } from '@angular/material';
+import { MatMenuTrigger, MatPaginator, MatSort } from '@angular/material';
 
 import { TranslateService } from '@ngx-translate/core';
 import { OneApiService } from 'src/app/api/one.service';
@@ -10,6 +9,7 @@ import { AlarmsService } from 'src/app/interaction/alarms.service';
 import { StateAccidentsDataSource } from './stateAccidentsDataSource';
 import { fromEvent, merge } from 'rxjs';
 import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { StateAccidentDto } from 'src/app/models/dtos/stateAccidentDto';
 
 @Component({
   selector: 'app-ft-state-accidents',
@@ -17,6 +17,8 @@ import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
   styleUrls: ['./ft-state-accidents.component.css']
 })
 export class FtStateAccidentsComponent implements OnInit, AfterViewInit {
+  labelPosition = "before";
+  sliderColor = "accent";
   isCurrentEvents: boolean;
 
   displayedColumns = [
@@ -24,11 +26,15 @@ export class FtStateAccidentsComponent implements OnInit, AfterViewInit {
     "eventRegistrationTimestamp",
     "rtuTitle",
     "traceTitle",
+    "state",
+    "explanation"
   ];
   dataSource: StateAccidentsDataSource;
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+  contextMenu: MatMenuTrigger;
+  contextMenuPosition = { x: "0px", y: "0px" };
 
   constructor(
     private oneApiService: OneApiService,
@@ -46,7 +52,7 @@ export class FtStateAccidentsComponent implements OnInit, AfterViewInit {
       String(this.isCurrentEvents),
       "desc",
       0,
-      8
+      13
     );
   }
 
@@ -72,6 +78,29 @@ export class FtStateAccidentsComponent implements OnInit, AfterViewInit {
     this.paginator.pageIndex = 0;
     console.log(`slider changed, isCurrentEvents ${this.isCurrentEvents}`);
     this.loadPage();
+  }
+  
+  onContextMenu(event: MouseEvent, row: StateAccidentDto) {
+    this.alarmsService.confirmOpticalEvent(row.id);
+
+    this.contextMenuPosition.x = event.clientX + "px";
+    this.contextMenuPosition.y = event.clientY + "px";
+    this.contextMenu.menuData = { row };
+    this.contextMenu.openMenu();
+    this.contextMenu.focus("mouse");
+    event.preventDefault();
+  }
+
+  getState(dto: StateAccidentDto){
+    return "state";
+  }
+
+  getExplanation(dto: StateAccidentDto){
+    return "explanation"
+  }
+
+  getBackground(dto : StateAccidentDto){
+    return "red";
   }
 
 }
