4436d7d1a4160669f1c0078f73503d65ac3a5d3b
mli2805 <mli2805@gmail.com>
2023-09-12 14:16:19 +0300

    get rid of TotalLoss duplicates

--- Shared/Utils35/SorData/AccidentsExtractor.cs
+++ Shared/Utils35/SorData/AccidentsExtractor.cs
@@ -43,6 +43,10 @@ namespace Iit.Fibertest.UtilsLib
                                       (a.OpticalTypeOfAccident == OpticalAccidentType.Reflectance || a.OpticalTypeOfAccident == OpticalAccidentType.Loss)))
                 return true;
 
+            if (accidentInSor.OpticalTypeOfAccident == OpticalAccidentType.TotalLoss &&
+                alreadyFound.Any(a=> a.OpticalTypeOfAccident == OpticalAccidentType.TotalLoss))
+                return true;
+
             return false;
         }
 
