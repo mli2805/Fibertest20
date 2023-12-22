b721b039e536c00bdf88601563343ac016bf0a2f
mli2805 <mli2805@gmail.com>
2023-12-22 14:27:00 +0300

    + Charon library for dotnet6

---
+++ RtuNet6/CharonLib/Charon/CharonOperationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Iit.Fibertest.CharonLib;
+
+public enum CharonOperationResult
+{
+    OtdrError = -9,
+    AdditionalOtauError = -2,
+    MainOtauError = -1,
+    LogicalError = 0,
+    Ok = 1,
+}
---
+++ msbuild.cmd
@@ -0,0 +1,6 @@
+ "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" Fibertest25.sln /p:Configuration=Release "/p:Platform=Any CPU"
+
+ rem "c:\Program Files\dotnet\dotnet.exe" publish -c Release Fibertest25.sln
+ 
+ pause
+ 
