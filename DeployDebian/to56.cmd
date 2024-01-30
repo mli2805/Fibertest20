06c07a2c4971ef60b85e3e9623b9afb8b6d12e82
mli2805 <mli2805@gmail.com>
2024-01-30 16:38:34 +0300

    new user guide

--- DeployDebian/to56.cmd
+++ DeployDebian/to56.cmd
@@ -2,9 +2,10 @@ rem In VS on project RtuDaemon press publish
 xcopy /E/D/Y c:\VsGitProjects\Fibertest20\RtuNetCore\RtuDaemon\bin\Release\net8.0\linux-x64\*.* rtu
 
 rem folder OtdrMeasEngine is prepared on ubuntu2004 virtual machine
+rem and place directly in /DeployDebian/rtu folder
 tar -czf rtu.tar.gz rtu
 
-rem password is iitft25user или 123
+rem password is iitft25user or 123
 c:\putty\pscp.exe rtu.tar.gz user@192.168.96.56:/var/tmp
 rem c:\putty\pscp.exe install.sh user@192.168.96.56:/var/tmp
 pause
