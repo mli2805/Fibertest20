f32874a0eddf49f0a656e6168d1c7856d8ce47af
mli2805 <mli2805@gmail.com>
2024-02-09 10:27:38 +0300

    NotifyUserCurrentMonitoringStep

--- DataCenter/DataCenterCore/RtuLinuxPollster/RtuLinuxPollsterThread.cs
+++ DataCenter/DataCenterCore/RtuLinuxPollster/RtuLinuxPollsterThread.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Iit.Fibertest.Dto;
 using Iit.Fibertest.Graph;
 using Iit.Fibertest.UtilsLib;
+using Iit.Fibertest.WcfConnections;
 
 namespace Iit.Fibertest.DataCenterCore
 {
@@ -13,26 +14,33 @@ namespace Iit.Fibertest.DataCenterCore
     {
         private readonly IniFile _iniFile;
         private readonly IMyLog _logFile;
+        private readonly ClientsCollection _clientsCollection;
         private readonly GlobalState _globalState;
         private readonly Model _writeModel;
         private readonly RtuStationsRepository _rtuStationsRepository;
         private readonly ClientToLinuxRtuHttpTransmitter _clientToLinuxRtuHttpTransmitter;
         private readonly MsmqMessagesProcessor _msmqMessagesProcessor;
+        private readonly D2CWcfManager _d2CWcfManager;
+        private readonly IFtSignalRClient _ftSignalRClient;
 
         private TimeSpan _gap;
         private readonly Dictionary<Guid, bool> _makLinuxRtuAccess = new Dictionary<Guid, bool>();
 
-        public RtuLinuxPollsterThread(IniFile iniFile, IMyLog logFile,
+        public RtuLinuxPollsterThread(IniFile iniFile, IMyLog logFile, ClientsCollection clientsCollection,
             GlobalState globalState, Model writeModel, RtuStationsRepository rtuStationsRepository,
-            ClientToLinuxRtuHttpTransmitter clientToLinuxRtuHttpTransmitter, MsmqMessagesProcessor msmqMessagesProcessor)
+            ClientToLinuxRtuHttpTransmitter clientToLinuxRtuHttpTransmitter,
+            MsmqMessagesProcessor msmqMessagesProcessor, D2CWcfManager d2CWcfManager, IFtSignalRClient ftSignalRClient)
         {
             _iniFile = iniFile;
             _logFile = logFile;
+            _clientsCollection = clientsCollection;
             _globalState = globalState;
             _writeModel = writeModel;
             _rtuStationsRepository = rtuStationsRepository;
             _clientToLinuxRtuHttpTransmitter = clientToLinuxRtuHttpTransmitter;
             _msmqMessagesProcessor = msmqMessagesProcessor;
+            _d2CWcfManager = d2CWcfManager;
+            _ftSignalRClient = ftSignalRClient;
         }
 
         public void Start()
@@ -113,10 +121,7 @@ namespace Iit.Fibertest.DataCenterCore
 
                     await _rtuStationsRepository.RegisterRtuHeartbeatAsync(heartbeatDto);
 
-                    //TODO Current Monitoring step
-
-
-                   
+                    await NotifyUserCurrentMonitoringStep(state.CurrentStepDto);
                 }
                 catch (Exception e)
                 {
@@ -134,6 +139,32 @@ namespace Iit.Fibertest.DataCenterCore
                 await _msmqMessagesProcessor.ProcessMonitoringResult(dto);
             }
 
+        }
+
+        private async Task NotifyUserCurrentMonitoringStep(CurrentMonitoringStepDto dto)
+        {
+            try
+            {
+                //                _logFile.AppendLine("WcfServiceForRtu.NotifyUserCurrentMonitoringStep: " + dto.CurrentStepDto);
+                if (_globalState.IsDatacenterInDbOptimizationMode)
+                    return;
+
+                if (_clientsCollection.HasAnyWebClients())
+                {
+                    await _ftSignalRClient.NotifyAll("NotifyMonitoringStep", dto.ToCamelCaseJson());
+                }
+
+                var addresses = _clientsCollection.GetAllDesktopClientsAddresses();
+                if (addresses == null)
+                    return;
+
+                _d2CWcfManager.SetClientsAddresses(addresses);
+                await _d2CWcfManager.NotifyUsersRtuCurrentMonitoringStep(dto);
+            }
+            catch (Exception e)
+            {
+                _logFile.AppendLine("RtuLinuxPollsterThread.NotifyUserCurrentMonitoringStep: " + e.Message);
+            }
         }
 
         private bool SaveResultInOrderToLogOnlyChanges(RtuCurrentStateDto state, Rtu makLinuxRtu)
