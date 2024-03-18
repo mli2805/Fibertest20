9a95765f54ec5d10dc6312cd03a462e59e64d8e6
mli2805 <mli2805@gmail.com>
2024-03-18 13:02:49 +0300

    deploy fixed

--- Deploy/PackSetup.cmd
+++ Deploy/PackSetup.cmd
@@ -52,6 +52,7 @@ del RftsReflect.zip
 cd ..\
 
 
+xcopy /E/D/Y ..\RtuNetCore\WatchDaemon\bin\Release\net8.0\linux-x64\publish\*.* PackLinuxRtu\
 xcopy /E/D/Y ..\RtuNetCore\RtuDaemon\bin\Release\net8.0\linux-x64\publish\*.* PackLinuxRtu\
 
 curl --user mli:iNansIM6Y8Uq http://192.168.96.4:8989/job/linux-projects/job/OtdrMeasEngine-Fibertest-linux64/pinned-for-ft30/artifact/otdrmeasengine.tar.gz --output PackLinuxRtu\otdrmeasengine.tar.gz
