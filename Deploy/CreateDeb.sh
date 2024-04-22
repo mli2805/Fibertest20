0a07dbb6ce169335b194586bd5079fe4f0d3654e
mli2805 <mli2805@gmail.com>
2024-04-22 15:28:41 +0300

    permissions

--- Deploy/CreateDeb.sh
+++ Deploy/CreateDeb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 chmod -R 755 "./rtu_linux"
+ls -l
 dpkg-deb --build "./rtu_linux" "./FtRtuLinux_${Version}.deb"
-chmod -R 777 "./FtRtuLinux_${Version}.deb"
+chmod -R 775 "./FtRtuLinux_${Version}.deb"
