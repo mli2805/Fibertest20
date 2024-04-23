e615a84fe32dc35284af8651bc963e15966670d0
mli2805 <mli2805@gmail.com>
2024-04-23 09:10:50 +0300

    cleanup

--- Deploy/CreateDeb.sh
+++ Deploy/CreateDeb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 chmod -R 755 "./rtu_linux"
-ls -l
 dpkg-deb --build "./rtu_linux" "./FtRtuLinux_${Version}.deb"
 chmod -R 775 "./FtRtuLinux_${Version}.deb"
--- Deploy/PackSetup.cmd
+++ Deploy/PackSetup.cmd
@@ -19,6 +19,7 @@ pause
 
 xcopy ..\Install\bin\Release\*.* Pack\bin\*.* /S/D/Y
 xcopy ..\Install\UserGuide\*.pdf Pack\UserGuide\*.* /S/D/Y
+xcopy ..\Uninstall\bin\Release\*.* Pack\UninstallFiles\*.* /S/D/Y
      
 xcopy ..\DataCenter\DataCenterService\bin\Release\*.* Pack\DcFiles\*.* /S/D/Y
 xcopy ..\DataCenter\DataCenterWebApi\bin\Release\netcoreapp3.1\*.* Pack\WebApi\*.* /S/D/Y
@@ -28,34 +29,33 @@ xcopy "..\Auxiliary Files\*.mib" Pack\DcFiles\*.* /S/D/Y
 
 xcopy ..\Client\WpfClient\bin\Release\*.* Pack\ClientFiles\*.* /S/D/Y
 xcopy "..\Auxiliary Files\*.rft" Pack\RftsTemplates\*.* /S/D/Y
-pause
 xcopy ..\Client\SuperClient\bin\Release\*.* Pack\SuperClientFiles\*.* /S/D/Y
-      
-xcopy ..\Uninstall\bin\Release\*.* Pack\UninstallFiles\*.* /S/D/Y
-
-rem RTU installer source
 
 xcopy ..\InstallRtu\bin\Release\*.* PackRtu\bin\*.* /S/D/Y
-     
+xcopy ..\Uninstall\bin\Release\*.* PackRtu\UninstallFiles\*.* /S/D/Y
+   
 xcopy ..\RTU\RtuService\bin\Release\*.* PackRtu\RtuFiles\*.* /S/D/Y
 xcopy ..\RTU\RtuWatchdog\bin\Release\*.* PackRtu\RtuFiles\*.* /S/D/Y
-rem OtdrMeasEngine folder will be created from Iit_Otdr_Vs build (Fibertest Itself dependecies) 
+rem On Teamcity OtdrMeasEngine folder will be created from Iit_Otdr_Vs build (Fibertest Itself dependecies) 
 rem files from Iit_Otdr_Vs will be copied from to Temp folder
 xcopy Temp\OtdrMeasEngine\*.* PackRtu\RtuFiles\OtdrMeasEngine\*.* /S/D/Y
 xcopy ..\Utils\*.* PackRtu\Utils\*.* /S/D/Y
- 
-xcopy ..\Uninstall\bin\Release\*.* PackRtu\UninstallFiles\*.* /S/D/Y
-
 
 rem get RftsReflect from Jenkins
-
 curl --user mli:iNansIM6Y8Uq http://192.168.96.4:8989/job/windows-projects/job/RFTSViewer/pinned-for-ft20/artifact/trunk/Source/RftsReflect.zip --output Pack\RftsReflect.zip
 cd Pack\
 ..\7z.exe x RftsReflect.zip 
 del RftsReflect.zip
 cd ..\
+rem both installers need RftsReflect
+xcopy Pack\RftsReflect\*.* PackRtu\RftsReflect\*.* /S/D/Y
 
-rem collect Linux RTU part
+rem Create self-extracted installers
+"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" Ft_%1.exe Pack\*.*
+"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackRtuSetup.conf" FtRtu_%1.exe PackRtu\*.*
+
+
+rem Linux RTU part
 xcopy /E/D/Y ..\RtuNetCore\WatchDaemon\bin\Release\net8.0\linux-x64\publish\*.* rtu_linux\var\fibertest\bin\
 xcopy /E/D/Y ..\RtuNetCore\RtuDaemon\bin\Release\net8.0\linux-x64\publish\*.* rtu_linux\var\fibertest\bin\
 
@@ -66,23 +66,11 @@ mkdir OtdrMeasEngine
 tar -xzf otdrmeasengine.tar.gz -C OtdrMeasEngine\
 del otdrmeasengine.tar.gz
 cd ../../../..
-
-rem both installers need RftsReflect
-xcopy Pack\RftsReflect\*.* PackRtu\RftsReflect\*.* /S/D/Y
-
-"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackSetup.conf" Ft_%1.exe Pack\*.*
-"C:\Program Files\WinRAR\winrar.exe" a -iiconinstall.ico -r -cfg- -sfx -z"PackRtuSetup.conf" FtRtu_%1.exe PackRtu\*.*
-
-rem tar -C PackLinuxRtu -czf ftlinux_%1.tar.gz .
+rem End of Linux RTU part (deb package will be created in docker)
 
 rem additional archive with administrative tools
-
 xcopy ..\Client\Licenser\bin\Release\*.* PackAdmin\Licenser\bin\*.* /S/D/Y
 xcopy ..\Client\DbMigrationWpf\bin\Release\*.* PackAdmin\DbMigrationWpf\bin\*.* /S/D/Y
 xcopy ..\Client\KadastrLoader\bin\Release\*.* PackAdmin\KadastrLoader\bin\*.* /S/D/Y
 xcopy ..\Client\Broadcaster\bin\Release\*.* PackAdmin\Broadcaster\bin\*.* /S/D/Y
 xcopy ..\Client\MapLoader\bin\Release\*.* PackAdmin\MapLoader\bin\*.* /S/D/Y
-
-dir
-
-pause
